2eb45c034596c499442ac3b9db74157809df8556
Zachary Huff <zach.huff.386@gmail.com>
2015-02-18 04:07:42 -0500

    Update decorators

--- dart/lib/decorators/decorators.dart
+++ dart/lib/decorators/decorators.dart
@@ -1,11 +1,13 @@
 library decorators;
 
 import 'package:blckur/decorators/noselect.dart' as noselect;
+import 'package:blckur/decorators/polymer.dart' as polymer;
 
 import 'package:angular/angular.dart' as ng;
 
 class DecoratorsMod extends ng.Module {
   DecoratorsMod() {
     this.bind(noselect.NoSelectDec);
+    this.bind(polymer.PolymerDec);
   }
 }
