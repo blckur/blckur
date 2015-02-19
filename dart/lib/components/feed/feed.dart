7f58f00e6ce2e947dcdb7306c3ced7a0a93dbb02
Zachary Huff <zach.huff.386@gmail.com>
2015-02-19 14:31:54 -0500

    Add feed component

---
+++ dart/lib/components/feed/feed.dart
@@ -0,0 +1,11 @@
+library feed_comp;
+
+import 'package:angular/angular.dart' show Component;
+
+@Component(
+  selector: 'feed',
+  templateUrl: 'packages/blckur/components/feed/feed.html',
+  cssUrl: 'packages/blckur/components/feed/feed.css'
+)
+class FeedComp {
+}
