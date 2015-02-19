0527222d1e7693c254223f319fb7d4f283a291fc
Zachary Huff <zach.huff.386@gmail.com>
2015-02-19 16:49:18 -0500

    Update components

--- dart/lib/components/components.dart
+++ dart/lib/components/components.dart
@@ -1,5 +1,7 @@
 library components;
 
+import 'package:blckur/components/feed/feed.dart'
+  as feed;
 import 'package:blckur/components/test/test.dart'
   as test;
 
@@ -7,6 +9,7 @@ import 'package:angular/angular.dart' as ng;
 
 class ComponentsMod extends ng.Module {
   ComponentsMod() {
+    this.bind(feed.FeedComp);
     this.bind(test.TestComp);
   }
 }
