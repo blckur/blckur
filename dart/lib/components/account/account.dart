00c3a946d8d69c681ee54cca8157e7cfac289edd
Zachary Huff <zach.huff.386@gmail.com>
2015-02-21 01:22:43 -0500

    Rename account component

---
+++ dart/lib/components/account/account.dart
@@ -0,0 +1,11 @@
+library account_comp;
+
+import 'package:angular/angular.dart' show Component;
+
+@Component(
+  selector: 'account',
+  templateUrl: 'packages/blckur/components/account/account.html',
+  cssUrl: 'packages/blckur/components/account/account.css'
+)
+class AccountComp {
+}
