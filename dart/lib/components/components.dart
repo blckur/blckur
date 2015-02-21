0244d8638ba7ee20e6fb3ae506206363a83058c9
Zachary Huff <zach.huff.386@gmail.com>
2015-02-21 01:19:08 -0500

    Add account item

--- dart/lib/components/components.dart
+++ dart/lib/components/components.dart
@@ -1,5 +1,7 @@
 library components;
 
+import 'package:blckur/components/account_item/account_item.dart'
+  as account_item;
 import 'package:blckur/components/brand_logo/brand_logo.dart'
   as brand_logo;
 import 'package:blckur/components/feed/feed.dart'
@@ -11,6 +13,7 @@ import 'package:angular/angular.dart' as ng;
 
 class ComponentsMod extends ng.Module {
   ComponentsMod() {
+    this.bind(account_item.AccountItemComp);
     this.bind(brand_logo.BrandLogoComp);
     this.bind(feed.FeedComp);
     this.bind(test.TestComp);
