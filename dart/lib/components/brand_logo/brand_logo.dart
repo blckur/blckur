3f03a60e14b67204194acb6d57b6ebccabfef4da
Zachary Huff <zach.huff.386@gmail.com>
2015-02-21 00:55:37 -0500

    Set styles in brand logo init

--- dart/lib/components/brand_logo/brand_logo.dart
+++ dart/lib/components/brand_logo/brand_logo.dart
@@ -1,17 +1,27 @@
 library brand_logo_comp;
 
 import 'package:angular/angular.dart' show Component, NgAttr;
+import 'package:angular/angular.dart' as ng;
+import 'dart:html' as dom;
 
 @Component(
   selector: 'brand-logo',
-  template: '<img src="{{iconUrl}}">',
-  cssUrl: 'packages/blckur/components/brand_logo/brand_logo.css'
+  template: '<img>'
 )
-class BrandLogoComp {
+class BrandLogoComp implements ng.ShadowRootAware {
   @NgAttr('icon')
   String icon;
 
+  @NgAttr('size')
+  String size;
+
   String get iconUrl {
     return '/img/${this.icon}.png';
+  }
+
+  void onShadowRoot(dom.ShadowRoot root) {
+    root.querySelector('img')
+      ..style.width = this.size == null ? '80px' : this.size
+      ..attributes['src'] = this.iconUrl;
   }
 }
