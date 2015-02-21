47d4c2e63f1bf9fd597b46e143377888806963ed
Zachary Huff <zach.huff.386@gmail.com>
2015-02-21 01:25:46 -0500

    Add account add component

---
+++ dart/lib/components/account_add/account_add.dart
@@ -0,0 +1,11 @@
+library account_add_comp;
+
+import 'package:angular/angular.dart' show Component;
+
+@Component(
+  selector: 'account-add',
+  templateUrl: 'packages/blckur/components/account_add/account_add.html',
+  cssUrl: 'packages/blckur/components/account_add/account_add.css'
+)
+class AccountAddComp {
+}
--- dart/lib/components/components.dart
+++ dart/lib/components/components.dart
@@ -2,6 +2,8 @@ library components;
 
 import 'package:blckur/components/account/account.dart'
   as account;
+import 'package:blckur/components/account_add/account_add.dart'
+  as account_add;
 import 'package:blckur/components/brand_logo/brand_logo.dart'
   as brand_logo;
 import 'package:blckur/components/feed/feed.dart'
@@ -14,6 +16,7 @@ import 'package:angular/angular.dart' as ng;
 class ComponentsMod extends ng.Module {
   ComponentsMod() {
     this.bind(account.AccountComp);
+    this.bind(account_add.AccountAddComp);
     this.bind(brand_logo.BrandLogoComp);
     this.bind(feed.FeedComp);
     this.bind(test.TestComp);
