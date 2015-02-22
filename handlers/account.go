569c0d27536c8be935f16da2ff8edb173ab01831
Zachary Huff <zach.huff.386@gmail.com>
2015-02-22 18:40:57 -0500

    Add id to account

--- handlers/account.go
+++ handlers/account.go
@@ -2,14 +2,17 @@ package handlers
 
 import (
     "github.com/gin-gonic/gin"
+    "labix.org/v2/mgo/bson"
 )
 
 type Account struct {
+    Id bson.ObjectId `json:"id" binding:"required"`
     Email string `json:"email" binding:"required"`
 }
 
 func account_get(c *gin.Context) {
     acct := &Account{
+        Id: bson.NewObjectId(),
         Email: "zach.huff.386@gmail.com",
     }
     c.JSON(200, acct)
