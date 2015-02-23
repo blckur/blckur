f02e3231937e721e48dd8834a8569734943a35f5
Zachary Huff <zach.huff.386@gmail.com>
2015-02-23 01:09:07 -0500

    Remove test model

--- dart/lib/models/models.dart
+++ dart/lib/models/models.dart
@@ -1,13 +1,11 @@
 library models;
 
-import 'package:blckur/models/test.dart' as tst;
 import 'package:blckur/models/user.dart' as usr;
 
 import 'package:angular/angular.dart' as ng;
 
 class ModelsMod extends ng.Module {
   ModelsMod() {
-    this.bind(tst.Test);
     this.bind(usr.User);
   }
 }
