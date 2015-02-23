30a63bc757690a2f9547cb83e5c161160f13f579
Zachary Huff <zach.huff.386@gmail.com>
2015-02-23 01:16:21 -0500

    Remove test collection

--- dart/lib/collections/collections.dart
+++ dart/lib/collections/collections.dart
@@ -1,13 +1,11 @@
 library collections;
 
 import 'package:blckur/collections/accounts.dart' as accts;
-import 'package:blckur/collections/tests.dart' as tsts;
 
 import 'package:angular/angular.dart' as ng;
 
 class CollectionsMod extends ng.Module {
   CollectionsMod() {
     this.bind(accts.Accounts);
-    this.bind(tsts.Tests);
   }
 }
