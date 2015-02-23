158485613b2c3305b2e2ba9b5837a162c3b27b9f
Zachary Huff <zach.huff.386@gmail.com>
2015-02-23 01:15:46 -0500

    Add account model

---
+++ dart/lib/models/account.dart
@@ -0,0 +1,25 @@
+library account_mod;
+
+import 'package:blckur/model.dart' as mdl;
+
+import 'package:angular/angular.dart' show Injectable;
+import 'package:angular/angular.dart' as ng;
+
+@Injectable()
+class Account extends mdl.Model {
+  @mdl.Attribute('id')
+  String id;
+
+  @mdl.Attribute('user_id')
+  String userId;
+
+  @mdl.Attribute('type')
+  String type;
+
+  @mdl.Attribute('identity')
+  String identity;
+
+  Account(ng.Http http) : super(http);
+
+  String url = 'http://localhost:3000/account';
+}
--- dart/lib/models/models.dart
+++ dart/lib/models/models.dart
@@ -1,11 +1,13 @@
 library models;
 
+import 'package:blckur/models/account.dart' as acct;
 import 'package:blckur/models/user.dart' as usr;
 
 import 'package:angular/angular.dart' as ng;
 
 class ModelsMod extends ng.Module {
   ModelsMod() {
+    this.bind(acct.Account);
     this.bind(usr.User);
   }
 }
