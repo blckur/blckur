f5496deb2a529e61ef65ed726f900f32b7e65151
Zachary Huff <zach.huff.386@gmail.com>
2015-02-23 01:16:08 -0500

    Add accounts collection

---
+++ dart/lib/collections/accounts.dart
@@ -0,0 +1,16 @@
+library accounts_col;
+
+import 'package:blckur/collection.dart' as collec;
+import 'package:blckur/models/account.dart' as acct;
+
+import 'package:angular/angular.dart' show Injectable;
+import 'package:angular/angular.dart' as ng;
+
+@Injectable()
+class Accounts extends collec.Collection {
+  Type model = acct.Account;
+
+  Accounts(ng.Http http) : super(http);
+
+  String url = 'http://localhost:3000/account';
+}
--- dart/lib/collections/collections.dart
+++ dart/lib/collections/collections.dart
@@ -1,11 +1,13 @@
 library collections;
 
+import 'package:blckur/collections/accounts.dart' as accts;
 import 'package:blckur/collections/tests.dart' as tsts;
 
 import 'package:angular/angular.dart' as ng;
 
 class CollectionsMod extends ng.Module {
   CollectionsMod() {
+    this.bind(accts.Accounts);
     this.bind(tsts.Tests);
   }
 }
