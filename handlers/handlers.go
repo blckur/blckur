886441e1cb1a05072611609f56060b3f1ab83378
Zachary Huff <zach.huff.386@gmail.com>
2015-02-22 19:19:36 -0500

    Add access control middlewear

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,6 +4,13 @@ import (
     "github.com/gin-gonic/gin"
 )
 
+func AccessControl() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
+    }
+}
+
 func Register(engine *gin.Engine) {
+    engine.Use(AccessControl())
     engine.GET("/user", user_get)
 }
