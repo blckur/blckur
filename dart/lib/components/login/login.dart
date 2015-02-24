e2e36766cf14bbee114c60049aa4ca0b2e428d5f
Zachary Huff <zach.huff.386@gmail.com>
2015-02-23 21:51:49 -0500

    Add login component

---
+++ dart/lib/components/login/login.dart
@@ -0,0 +1,11 @@
+library login_comp;
+
+import 'package:angular/angular.dart' show Component;
+
+@Component(
+  selector: 'x-login',
+  templateUrl: 'packages/blckur/components/login/login.html',
+  cssUrl: 'packages/blckur/components/login/login.css'
+)
+class LoginComp {
+}
