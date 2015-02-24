cb35a7ae2aad76254857b83c8969b751ab6bdf78
Zachary Huff <zach.huff.386@gmail.com>
2015-02-23 21:52:07 -0500

    Add login to components

--- dart/lib/components/components.dart
+++ dart/lib/components/components.dart
@@ -10,6 +10,8 @@ import 'package:blckur/components/feed/feed.dart'
   as feed;
 import 'package:blckur/components/user/user.dart'
   as user;
+import 'package:blckur/components/login/login.dart'
+  as login;
 
 import 'package:angular/angular.dart' as ng;
 
@@ -20,5 +22,6 @@ class ComponentsMod extends ng.Module {
     this.bind(brand_logo.BrandLogoComp);
     this.bind(feed.FeedComp);
     this.bind(user.UserComp);
+    this.bind(login.LoginComp);
   }
 }
