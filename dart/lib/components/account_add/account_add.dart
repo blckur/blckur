fc164855d1b581386a001a28eebf554b96bf9ea3
Zachary Huff <zach.huff.386@gmail.com>
2015-02-23 19:47:40 -0500

    Add account types collection to account add component

--- dart/lib/components/account_add/account_add.dart
+++ dart/lib/components/account_add/account_add.dart
@@ -1,5 +1,8 @@
 library account_add_comp;
 
+import 'package:blckur/collections/account_types.dart' as acct_typs;
+import 'package:blckur/logger.dart' as logger;
+
 import 'package:angular/angular.dart' show Component, NgTwoWay;
 
 @Component(
@@ -8,8 +11,16 @@ import 'package:angular/angular.dart' show Component, NgTwoWay;
   cssUrl: 'packages/blckur/components/account_add/account_add.css'
 )
 class AccountAddComp {
+  acct_typs.AccountTypes accountTypes;
+
   @NgTwoWay('add-account')
   bool addAccount;
+
+  AccountAddComp(this.accountTypes) {
+    this.accountTypes.fetch().catchError((err) {
+      logger.severe('Failed to load account types', err);
+    });
+  }
 
   void onAccountAdd() {
     this.addAccount = true;
