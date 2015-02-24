7770392d59bc999b9e0f8675f4433ec7d6896425
Zachary Huff <zach.huff.386@gmail.com>
2015-02-24 02:27:16 -0500

    Use input component in login

--- dart/lib/components/login/login.dart
+++ dart/lib/components/login/login.dart
@@ -1,5 +1,8 @@
 library login_comp;
 
+import 'package:blckur/models/auth.dart' as ath;
+import 'package:blckur/logger.dart' as logger;
+
 import 'package:angular/angular.dart' show Component;
 
 @Component(
@@ -8,4 +11,25 @@ import 'package:angular/angular.dart' show Component;
   cssUrl: 'packages/blckur/components/login/login.css'
 )
 class LoginComp {
+  ath.Auth model;
+  String emailError;
+  String passwordError;
+
+  LoginComp(this.model);
+
+  void onLogin() {
+    this.model.create().then((_) {
+    }).catchError((err) {
+      logger.severe('Failed to load accounts', err);
+
+      if (err.error == 'auth_email_invalid') {
+        this.emailError = 'Email is invalid';
+        this.passwordError = null;
+      }
+      else if (err.error == 'auth_password_invalid') {
+        this.passwordError = 'Password is invalid';
+        this.emailError = null;
+      }
+    });
+  }
 }
