0b7ee07e2798fef497bd45a8098785e015e2a8ef
Zachary Huff <zach.huff.386@gmail.com>
2015-02-24 02:33:12 -0500

    Clear input error on change event

--- dart/lib/components/input/input.dart
+++ dart/lib/components/input/input.dart
@@ -1,13 +1,15 @@
 library input_comp;
 
 import 'package:angular/angular.dart' show Component, NgAttr, NgTwoWay;
+import 'package:angular/angular.dart' as ng;
+import 'dart:html' as dom;
 
 @Component(
   selector: 'x-input',
   templateUrl: 'packages/blckur/components/input/input.html',
   cssUrl: 'packages/blckur/components/input/input.css'
 )
-class InputComp {
+class InputComp implements ng.ShadowRootAware {
   String label;
   String labelClass;
   bool labelFloat;
@@ -34,19 +36,27 @@ class InputComp {
   var _error;
   @NgTwoWay('error')
   void set error(String val) {
-    this._error = val;
-    if (val == null) {
-      this.label = this.placeholder;
-      this.labelClass = null;
-      this.labelFloat = false;
-    }
-    else {
-      this.label = val;
-      this.labelClass = 'invalid';
-      this.labelFloat = true;
+    if (this._error != val) {
+      this._error = val;
+      if (val == null) {
+        this.label = this.placeholder;
+        this.labelClass = null;
+        this.labelFloat = false;
+      }
+      else {
+        this.label = val;
+        this.labelClass = 'invalid';
+        this.labelFloat = true;
+      }
     }
   }
   String get error {
     return this._error;
+  }
+
+  void onShadowRoot(dom.ShadowRoot root) {
+    root.querySelector('input').onInput.listen((_) {
+      this.error = null;
+    });
   }
 }
