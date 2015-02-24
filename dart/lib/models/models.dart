f15221198f90f0128e25b6a27d55a2a9e64c6264
Zachary Huff <zach.huff.386@gmail.com>
2015-02-24 02:04:00 -0500

    Add auth to models

--- dart/lib/models/models.dart
+++ dart/lib/models/models.dart
@@ -2,6 +2,7 @@ library models;
 
 import 'package:blckur/models/account.dart' as acct;
 import 'package:blckur/models/account_type.dart' as acct_typ;
+import 'package:blckur/models/auth.dart' as ath;
 import 'package:blckur/models/event.dart' as evnt;
 import 'package:blckur/models/user.dart' as usr;
 
@@ -11,6 +12,7 @@ class ModelsMod extends ng.Module {
   ModelsMod() {
     this.bind(acct.Account);
     this.bind(acct_typ.AccountType);
+    this.bind(ath.Auth);
     this.bind(evnt.Event);
     this.bind(usr.User);
   }
