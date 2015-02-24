c39d285c4dae01795bec5ed7f281827211e2b5d4
Zachary Huff <zach.huff.386@gmail.com>
2015-02-24 16:56:11 -0500

    Init database in main

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
     "github.com/blckur/blckur/handlers"
+    "github.com/blckur/blckur/database"
     "github.com/gin-gonic/gin"
 )
 
 func main() {
+    database.Connect()
+
     router := gin.Default()
     handlers.Register(router)
     router.Run(":3000")
