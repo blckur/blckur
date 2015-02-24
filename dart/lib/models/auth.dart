0f4eebcffb5e7b861cd34cfd586363363be355ac
Zachary Huff <zach.huff.386@gmail.com>
2015-02-24 02:03:13 -0500

    Add auth model

---
+++ dart/lib/models/auth.dart
@@ -0,0 +1,33 @@
+library auth_mod;
+
+import 'package:blckur/model.dart' as mdl;
+
+import 'package:angular/angular.dart' show Injectable;
+import 'package:angular/angular.dart' as ng;
+
+@Injectable()
+class Auth extends mdl.Model {
+  @mdl.Attribute('email')
+  String email;
+
+  @mdl.Validator('email')
+  void emailValidator(val) {
+    if (val == null || val == '') {
+      throw new mdl.Invalid('empty', 'Email cannot be empty');
+    }
+  }
+
+  @mdl.Attribute('password')
+  String password;
+
+  @mdl.Validator('password')
+  void passwordValidator(val) {
+    if (val == null || val == '') {
+      throw new mdl.Invalid('empty', 'Password cannot be empty');
+    }
+  }
+
+  Auth(ng.Http http) : super(http);
+
+  String url = 'http://localhost:3000/auth';
+}
