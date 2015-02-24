d2afcfa0a7af8030fbcc0fd80d9e2f6e30ce24c8
Zachary Huff <zach.huff.386@gmail.com>
2015-02-23 19:58:50 -0500

    Add cors handler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -6,11 +6,37 @@ import (
 
 func AccessControl() gin.HandlerFunc {
     return func(c *gin.Context) {
-        c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
+        headers := c.Writer.Header()
+
+        headers.Add("Access-Control-Allow-Origin", "*")
+        headers.Add("Access-Control-Allow-Methods",
+            "GET,PUT,POST,DELETE")
+        headers.Add("Access-Control-Max-Age", "43200")
+        headers.Add("Access-Control-Allow-Headers",
+            "Authorization,Content-Type,Accept,Origin,User-Agent,DNT," +
+            "Cache-Control,X-Mx-ReqToken,Keep-Alive,X-Requested-With," +
+            "If-Modified-Since")
+
+        if (c.Request.Method == "OPTIONS") {
+            c.AbortWithStatus(200)
+        } else {
+            c.Next()
+        }
     }
 }
 
 func Register(engine *gin.Engine) {
-    engine.Use(AccessControl())
-    engine.GET("/user", userGet)
+    corsGroup := engine.Group("")
+    corsGroup.Use(AccessControl())
+
+    corsGroup.GET("/account_types", accountsTypesGet)
+
+    corsGroup.GET("/accounts", accountsGet)
+    //corsGroup.GET("/accounts/detail", todo)
+
+    corsGroup.GET("/user", userGet)
+    corsGroup.PUT("/user", userPut)
+    corsGroup.OPTIONS("/user", nil)
+
+    engine.GET("/events", eventGet)
 }
