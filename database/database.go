55227d32d776d0512e822fb7189e329fba92a2f0
Zachary Huff <zach.huff.386@gmail.com>
2015-02-26 13:39:21 -0500

    Add parse error to database

--- database/database.go
+++ database/database.go
@@ -78,3 +78,38 @@ func Init() (err error) {
 
     return
 }
+
+func GetErrorCode(err error) (errCode int) {
+    switch err := err.(type) {
+    case *mgo.LastError:
+        errCode = err.Code;
+    case *mgo.QueryError:
+        errCode = err.Code;
+    }
+
+    return
+}
+
+func ParseError(err error) (newErr error) {
+    if err == mgo.ErrNotFound {
+        newErr = &NotFoundError{
+            errors.New("database: Not found"),
+        }
+        return
+    }
+
+    errCode := GetErrorCode(err)
+
+    switch errCode {
+    case 11000, 11001, 12582, 16460:
+        newErr = &DuplicateKeyError{
+            errors.New("database: Duplicate key"),
+        }
+    default:
+        newErr = &UnknownError{
+            errors.New("database: Unknown error"),
+        }
+    }
+
+    return
+}
--- database/errors.go
+++ database/errors.go
@@ -12,6 +12,14 @@ type IndexError struct {
     errors.DropboxError
 }
 
+type NotFoundError struct {
+    errors.DropboxError
+}
+
+type DuplicateKeyError struct {
+    errors.DropboxError
+}
+
 type UnknownError struct {
     errors.DropboxError
 }
