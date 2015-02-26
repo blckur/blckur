a8601f7e1970d636f0ae6c26bb3959b3696cdf58
Zachary Huff <zach.huff.386@gmail.com>
2015-02-26 01:02:30 -0500

    Init auth

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
     "github.com/blckur/blckur/handlers"
     "github.com/blckur/blckur/database"
+    "github.com/blckur/blckur/auth"
     "github.com/gin-gonic/gin"
 )
 
@@ -12,7 +13,13 @@ func main() {
         panic(err)
     }
 
+    err = auth.Init()
+    if err != nil {
+        panic(err)
+    }
+
     router := gin.Default()
     handlers.Register(router)
+
     router.Run(":3000")
 }
