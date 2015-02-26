fce3ff31c6dd4c26ef1df7ae86d0c95fb4430b6d
Zachary Huff <zach.huff.386@gmail.com>
2015-02-26 01:40:51 -0500

    Add signup to login component

--- dart/lib/components/login/login.dart
+++ dart/lib/components/login/login.dart
@@ -19,9 +19,29 @@ class LoginComp {
   LoginComp(this.model) : mode = 'login';
 
   void onLogin() {
-    this.model.create().then((_) {
+    this.model.login().then((_) {
+      this.emailError = null;
+      this.passwordError = null;
     }).catchError((err) {
-      logger.severe('Failed to load accounts', err);
+      logger.severe('Failed to login', err);
+
+      if (err.error == 'auth_email_invalid') {
+        this.emailError = 'Email is invalid';
+        this.passwordError = null;
+      }
+      else if (err.error == 'auth_password_invalid') {
+        this.passwordError = 'Password is invalid';
+        this.emailError = null;
+      }
+    });
+  }
+
+  void onSignup() {
+    this.model.signup().then((_) {
+      this.emailError = null;
+      this.passwordError = null;
+    }).catchError((err) {
+      logger.severe('Failed to signup', err);
 
       if (err.error == 'auth_email_invalid') {
         this.emailError = 'Email is invalid';
