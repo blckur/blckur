cccfb5ee0e580ff9b66b4d315d6d3564dbf599c6
Zachary Huff <zach.huff.386@gmail.com>
2015-02-26 21:21:20 -0500

    Add parse email

--- utils/utils.go
+++ utils/utils.go
@@ -1,11 +1,26 @@
 package utils
 
 import (
+    "github.com/dropbox/godropbox/errors"
     "crypto/rand"
+    "net/mail"
 )
 
 func RandBytes(size int) (bytes []byte, err error) {
     bytes = make([]byte, 32)
     _, err = rand.Read(bytes)
+    return
+}
+
+func ParseEmail(input string) (email string, err error) {
+    address, err := mail.ParseAddress(input)
+    if err != nil {
+        err = &InvalidEmailError{
+            errors.Wrap(err, "utils: Invalid email"),
+        }
+        return
+    }
+    email = address.Address
+
     return
 }
