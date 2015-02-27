2500abc95aee3ebc06caa7233c8ab49895e42ce1
Zachary Huff <zach.huff.386@gmail.com>
2015-02-27 02:38:06 -0500

    Remove settings from router

--- dart/lib/routers/main.dart
+++ dart/lib/routers/main.dart
@@ -1,25 +1,17 @@
 library main_rout;
 
-import 'package:blckur/settings/settings.dart' as settings;
-
 import 'package:angular/angular.dart' as ng;
 
 MainRout(ng.Router router, ng.RouteViewFactory views) {
   views.configure({
     'root': ng.ngRoute(
-      path: '/root',
+      path: '/feed',
       view: 'packages/blckur/views/root.html',
-      enter: (_) {
-        settings.set('active_page', 'root');
-      },
       defaultRoute: true
     ),
     'login': ng.ngRoute(
       path: '/login',
-      view: 'packages/blckur/views/login.html',
-      enter: (_) {
-        settings.set('active_page', 'login');
-      }
+      view: 'packages/blckur/views/login.html'
     )
   });
 }
