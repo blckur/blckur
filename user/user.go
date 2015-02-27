a4ffd6b39b534ec36cd7ff1284b7e9929330e624
Zachary Huff <zach.huff.386@gmail.com>
2015-02-27 17:48:50 -0500

    Change struct in user

--- user/user.go
+++ user/user.go
@@ -6,6 +6,7 @@ import (
 	"github.com/blckur/blckur/database"
 	"github.com/blckur/blckur/errortypes"
 	"github.com/blckur/blckur/utils"
+	"github.com/dropbox/godropbox/container/set"
 	"github.com/dropbox/godropbox/errors"
 	"labix.org/v2/mgo/bson"
 	"math/rand"
@@ -13,11 +14,12 @@ import (
 )
 
 type User struct {
-	Id           bson.ObjectId `bson:"_id,omitempty" json:"id" binding:"required"`
+	Id           bson.ObjectId `bson:"_id,omitempty"
+		json:"id" binding:"required"`
 	Email        string        `bson:"email" json:"email" binding:"required"`
 	PasswordSalt []byte        `bson:"password_salt" json:"-"`
 	PasswordHash []byte        `bson:"password_hash" json:"-"`
-	db           *database.Database
+	*database.Collection
 }
 
 func (u *User) hashPassword(password string) []byte {
@@ -56,10 +58,8 @@ func (u *User) SetPassword(password string) (err error) {
 	return
 }
 
-func (u *User) Commit(db *database.Database) (err error) {
-	usrsCol := db.Users()
-
-	err = usrsCol.UpdateId(u.Id, bson.M{
+func (u *User) Commit() (err error) {
+	err = u.UpdateId(u.Id, bson.M{
 		"$set": u,
 	})
 	if err != nil {
@@ -71,12 +71,10 @@ func (u *User) Commit(db *database.Database) (err error) {
 }
 
 func FindUser(db *database.Database, email string) (usr *User, err error) {
-	usrsCol := db.Users()
-	usr = &User{
-		db: db,
-	}
+	coll := db.Users()
+	usr = &User{}
 
-	err = usrsCol.Find(bson.M{
+	err = coll.Find(bson.M{
 		"email": email,
 	}).One(usr)
 	if err != nil {
@@ -84,36 +82,38 @@ func FindUser(db *database.Database, email string) (usr *User, err error) {
 		return
 	}
 
+	usr.Collection = coll
+
 	return
 }
 
 func GetUser(db *database.Database, id bson.ObjectId) (usr *User, err error) {
-	usrsCol := db.Users()
-	usr = &User{
-		db: db,
-	}
+	coll := db.Users()
+	usr = &User{}
 
-	err = usrsCol.FindId(id).One(usr)
+	err = coll.FindId(id).One(usr)
 	if err != nil {
 		err = database.ParseError(err)
 		return
 	}
 
+	usr.Collection = coll
+
 	return
 }
 
 func NewUser(db *database.Database, email string, password string) (
-	usr *User, err error) {
-	usrsCol := db.Users()
+		usr *User, err error) {
+	coll := db.Users()
 
 	usr = &User{
 		Id:    bson.NewObjectId(),
 		Email: email,
-		db:    db,
+		Collection: coll,
 	}
 	usr.SetPassword(password)
 
-	err = usrsCol.Insert(usr)
+	err = coll.Insert(usr)
 	if err != nil {
 		err = database.ParseError(err)
 		return
