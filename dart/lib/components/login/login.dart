b60079c9df651a4ad5695af85e0b3e03703c7dd6
Zachary Huff <zach.huff.386@gmail.com>
2015-02-26 21:41:33 -0500

    Add signup to login comonent

--- dart/lib/components/login/login.dart
+++ dart/lib/components/login/login.dart
@@ -4,6 +4,8 @@ import 'package:blckur/models/auth.dart' as ath;
 import 'package:blckur/logger.dart' as logger;
 
 import 'package:angular/angular.dart' show Component;
+import 'package:angular/angular.dart' as ng;
+import 'dart:async' as async;
 
 @Component(
   selector: 'x-login',
@@ -15,24 +17,35 @@ class LoginComp {
   String emailError;
   String passwordError;
   String mode;
+  ng.Router router;
 
-  LoginComp(this.model) : mode = 'login';
+  LoginComp(this.model, this.router) : mode = 'login';
+
+  void _handlerError(async.AsyncError err) {
+    if (err.error == 'email_invalid') {
+      this.emailError = 'Email is invalid';
+      this.passwordError = null;
+    }
+    else if (err.error == 'password_invalid') {
+      this.passwordError = 'Password is invalid';
+      this.emailError = null;
+    }
+    else if (err.error == 'email_exists') {
+      this.emailError = 'Email is already signed up';
+      this.passwordError = null;
+    }
+  }
 
   void onLogin() {
     this.model.login().then((_) {
       this.emailError = null;
       this.passwordError = null;
+      this.router.gotoUrl('/root').then((_) {
+        this.model.clear();
+      });
     }).catchError((err) {
       logger.severe('Failed to login', err);
-
-      if (err.error == 'auth_email_invalid') {
-        this.emailError = 'Email is invalid';
-        this.passwordError = null;
-      }
-      else if (err.error == 'auth_password_invalid') {
-        this.passwordError = 'Password is invalid';
-        this.emailError = null;
-      }
+      this._handlerError(err);
     });
   }
 
@@ -40,17 +53,12 @@ class LoginComp {
     this.model.signup().then((_) {
       this.emailError = null;
       this.passwordError = null;
+      this.router.gotoUrl('/root').then((_) {
+        this.model.clear();
+      });
     }).catchError((err) {
       logger.severe('Failed to signup', err);
-
-      if (err.error == 'auth_email_invalid') {
-        this.emailError = 'Email is invalid';
-        this.passwordError = null;
-      }
-      else if (err.error == 'auth_password_invalid') {
-        this.passwordError = 'Password is invalid';
-        this.emailError = null;
-      }
+      this._handlerError(err);
     });
   }
 }
