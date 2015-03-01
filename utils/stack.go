314a83cd1616b77055f6a2a27743c56cc2709d9f
Zachary Huff <zach.huff.386@gmail.com>
2015-03-01 14:29:34 -0500

    Add string stack

---
+++ utils/stack.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"sync"
+)
+
+type StringStack struct {
+	data []string
+	mutex *sync.Mutex
+}
+
+func (s *StringStack) Push(elem string) {
+	s.mutex.Lock()
+	append(s.data, elem)
+	s.mutex.Unlock()
+}
+
+func (s *StringStack) Pop() (elem string) {
+	s.mutex.Lock()
+	n := len(s.data)
+	elem = s.data[n - 1]
+	s.data = s.data[:n -1]
+	s.mutex.Unlock()
+	return
+}
+
+func NewStringStack(data []string) (que *StringStack) {
+	que = &StringStack{
+		data: data,
+		mutex: &sync.Mutex{},
+	}
+	return
+}
