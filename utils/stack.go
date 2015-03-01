11a4d82c487ff0707d464797f6889401fd94f95f
Zachary Huff <zach.huff.386@gmail.com>
2015-03-01 14:38:05 -0500

    Check 0 len in stack

--- utils/stack.go
+++ utils/stack.go
@@ -17,9 +17,15 @@ func (s *StringStack) Push(elem string) {
 
 func (s *StringStack) Pop() (elem string) {
 	s.mutex.Lock()
+
 	n := len(s.data)
+	if n == 0 {
+		return
+	}
+
 	elem = s.data[n - 1]
 	s.data = s.data[:n -1]
+
 	s.mutex.Unlock()
 	return
 }
