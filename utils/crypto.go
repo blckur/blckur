fa912b564ad9ba1a23694c2d998aaf3cb3abbe1c
Zachary Huff <zach.huff.386@gmail.com>
2015-03-02 02:14:28 -0500

    Add seed rand

--- utils/crypto.go
+++ utils/crypto.go
@@ -4,6 +4,8 @@ import (
 	"crypto/rand"
 	"github.com/blckur/blckur/errortypes"
 	"github.com/dropbox/godropbox/errors"
+	"math/big"
+	mathrand "math/rand"
 )
 
 func RandBytes(size int) (bytes []byte, err error) {
@@ -15,6 +17,20 @@ func RandBytes(size int) (bytes []byte, err error) {
 		}
 		return
 	}
+
+	return
+}
+
+func SeedRand() (err error) {
+	n, err := rand.Int(rand.Reader, big.NewInt(9223372036854775806))
+	if err != nil {
+		err = &errortypes.UnknownError{
+			errors.Wrap(err, "utils: Random seed error"),
+		}
+		return
+	}
+
+	mathrand.Seed(n.Int64())
 
 	return
 }
