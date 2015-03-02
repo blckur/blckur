0cfe1b3011beb8ba3c7251c1674cc47ab059d457
Zachary Huff <zach.huff.386@gmail.com>
2015-03-01 20:09:38 -0500

    Add queue streams

--- queue/queue.go
+++ queue/queue.go
@@ -7,7 +7,7 @@ import (
 	"github.com/kr/beanstalk"
 	"labix.org/v2/mgo/bson"
 	"time"
-//	"log"
+	"log"
 	"github.com/blckur/blckur/utils"
 	"sync"
 	"fmt"
@@ -211,42 +211,61 @@ func (q *Queue) Put(data interface{}, priority uint32,
 	return
 }
 
-func (q *Queue) Reserve(timeout time.Duration) (job *Job) {
+type QueueStream struct {
+	server string
+	queue *Queue
+}
+
+func (q *QueueStream) Reserve(timeout time.Duration) (job *Job) {
 	for {
-		for _, server := range q.servers {
-			conn, err := q.conn(server)
-			if err != nil {
-				//log.Printf("ERROR: queue: %s", err.Error())
+		conn, err := q.queue.conn(q.server)
+		if err != nil {
+			log.Printf("ERROR: queue: %s", err.Error())
+			continue
+		}
+
+		id, body, err := conn.Reserve(timeout)
+		if err != nil {
+			if err.Error() == "reserve-with-timeout: timeout" {
 				continue
 			}
+			log.Printf("ERROR: queue: %s", err.Error())
+			q.queue.close(q.server)
+			continue
+		}
 
-			id, body, err := conn.Reserve(timeout)
-			if err != nil {
-				if err.Error() == "reserve-with-timeout: timeout" {
-					continue
-				}
-				//log.Printf("ERROR: queue: %s", err.Error())
-				q.close(server)
-				continue
-			}
+		jobData := &JobData{}
+		err = json.Unmarshal(body, jobData)
+		if err != nil {
+			log.Printf("ERROR: queue: %s", err.Error())
+			continue
+		}
 
-			jobData := &JobData{}
-			err = json.Unmarshal(body, jobData)
-			if err != nil {
-				//log.Printf("ERROR: queue: %s", err.Error())
-				continue
-			}
+		job = &Job{
+			beanId: id,
+			conn: conn,
+			Id: jobData.Id,
+			Type: jobData.Type,
+			Data: jobData.Data,
+		}
 
-			job = &Job{
-				beanId: id,
-				conn: conn,
-				Id: jobData.Id,
-				Type: jobData.Type,
-				Data: jobData.Data,
-			}
-			return
+		return
+	}
+
+	return
+}
+
+func (q *Queue) GetStreams() (streams []*QueueStream) {
+	streams = make([]*QueueStream, len(q.servers))
+
+	for i, server := range q.servers {
+		streams[i] = &QueueStream{
+			server: server,
+			queue: q,
 		}
 	}
+
+	return
 }
 
 func NewQueue(servers []string, consistency int) (que *Queue) {
