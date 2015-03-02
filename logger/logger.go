5fc23766943366c1f3378bcfe2629059bf9c58da
Zachary Huff <zach.huff.386@gmail.com>
2015-03-02 18:32:58 -0500

    Add funcs to logger

--- logger/logger.go
+++ logger/logger.go
@@ -10,8 +10,32 @@ var (
 	log = logging.MustGetLogger("blckur")
 	format = logging.MustStringFormatter(
 		"%{color:bold}%{time:06/01/02 15:04:05} " +
-		"%{shortfunc} %{level:.4s} ▶%{color:reset} %{message}")
+		"%{level:.4s} ▶%{color:reset} %{message}")
 )
+
+func Debug(format string, args ...interface{}) {
+	log.Debug(format, args...)
+}
+
+func Info(format string, args ...interface{}) {
+	log.Info(format, args...)
+}
+
+func Notice(format string, args ...interface{}) {
+	log.Notice(format, args...)
+}
+
+func Warning(format string, args ...interface{}) {
+	log.Warning(format, args...)
+}
+
+func Error(format string, args ...interface{}) {
+	log.Error(format, args...)
+}
+
+func Critical(format string, args ...interface{}) {
+	log.Critical(format, args...)
+}
 
 func Init() (err error) {
 	utils.After("database")
