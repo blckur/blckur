e5158e249ba4897df4ee70d87bc6578f29a2a8d0
Zachary Huff <zach.huff.386@gmail.com>
2015-03-02 02:34:45 -0500

    Add set cmd to main

--- main.go
+++ main.go
@@ -4,32 +4,56 @@ import (
 	"github.com/blckur/blckur/database"
 	"github.com/blckur/blckur/handlers"
 	"github.com/blckur/blckur/session"
+	"github.com/blckur/blckur/settings"
+	"github.com/blckur/blckur/utils"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"time"
+	"flag"
 )
 
 func main() {
+	flag.Parse()
+
 	err := database.Init()
 	if err != nil {
 		panic(err)
 	}
 
-	err = session.Init()
+	err = settings.Init()
 	if err != nil {
 		panic(err)
 	}
 
-	router := gin.Default()
-	handlers.Register(router)
+	switch flag.Arg(0) {
+	case "set":
+		group := flag.Arg(1)
+		key := flag.Arg(2)
+		val := flag.Arg(3)
+		db := database.GetDatabase()
 
-	server := &http.Server{
-		Addr: ":80",
-		Handler: router,
-		ReadTimeout: 10 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		MaxHeaderBytes: 4096,
+		err = settings.Set(db, group, key, val)
+		if err != nil {
+			panic(err)
+		}
+		return
+	case "start":
+		err = session.Init()
+		if err != nil {
+			panic(err)
+		}
+
+		router := gin.Default()
+		handlers.Register(router)
+
+		server := &http.Server{
+			Addr: ":80",
+			Handler: router,
+			ReadTimeout: 10 * time.Second,
+			WriteTimeout: 10 * time.Second,
+			MaxHeaderBytes: 4096,
+		}
+
+		server.ListenAndServe()
 	}
-
-	server.ListenAndServe()
 }
