e7c01e718f33d7bec0de92900144da5a03099779
Zachary Huff <zach.huff.386@gmail.com>
2015-03-03 15:41:13 -0500

    Add get local address to utils

--- utils/utils.go
+++ utils/utils.go
@@ -2,8 +2,11 @@ package utils
 
 import (
 	"math/rand"
+	"github.com/blckur/blckur/errortypes"
 	"github.com/dropbox/godropbox/errors"
+	"net"
 	"net/mail"
+	"os"
 )
 
 func ParseEmail(input string) (email string, err error) {
@@ -30,5 +33,27 @@ func ShuffleStringsNew(data []string) (randData []string) {
 	randData = make([]string, len(data))
 	copy(randData, data)
 	ShuffleStrings(randData)
+	return
+}
+
+func GetLocalAddress() (addr string, err error) {
+	name, err := os.Hostname()
+	if err != nil {
+		err = &errortypes.UnknownError{
+			errors.Wrap(err, "beanstalkd: Get ip"),
+		}
+		return
+	}
+
+	addrs, err := net.LookupHost(name)
+	if err != nil {
+		err = &errortypes.UnknownError{
+			errors.Wrap(err, "beanstalkd: Get ip"),
+		}
+		return
+	}
+
+	addr = addrs[0]
+
 	return
 }
