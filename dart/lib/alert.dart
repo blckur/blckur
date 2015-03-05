ea663ed0822270811e0c5ac8aa903439a2b3afc8
Zachary Huff <zach.huff.386@gmail.com>
2015-03-04 21:48:05 -0500

    Add alert

---
+++ dart/lib/alert.dart
@@ -0,0 +1,38 @@
+library alert;
+
+import 'dart:collection' as collection;
+import 'dart:async' as async;
+
+collection.Queue<Alert> alerts = new collection.Queue();
+
+class Alert {
+  Function retryCallback;
+  String text;
+
+  bool _opened;
+  bool get opened {
+    return this._opened;
+  }
+  void set opened(bool val) {
+    this._opened = val;
+    if (!val) {
+      new async.Timer(const Duration(milliseconds: 100), () {
+        alerts.remove(this);
+      });
+    }
+  }
+  
+  Alert(this.text, [this.retryCallback]) {
+    this.opened = true;
+
+    alerts.add(this);
+
+    while (alerts.length > 3) {
+      alerts.removeFirst();
+    }
+  }
+}
+
+void clear() {
+  alerts.clear();
+}
