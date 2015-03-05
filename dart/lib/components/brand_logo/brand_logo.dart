0903a1ee61e5aac89436278f6c3fcde786fbef97
Zachary Huff <zach.huff.386@gmail.com>
2015-03-04 21:51:40 -0500

    Improve brand logo

--- dart/lib/components/brand_logo/brand_logo.dart
+++ dart/lib/components/brand_logo/brand_logo.dart
@@ -10,22 +10,51 @@ import 'dart:html' as dom;
   template: '<img>'
 )
 class BrandLogoComp extends all_aware.AllAware {
-  @NgAttr('icon')
-  String icon;
+  dom.Element imgElem;
 
+  var _icon;
+  @NgAttr('icon')
+  String get icon {
+    return this._icon;
+  }
+  void set icon(String val) {
+    if (val != this._icon) {
+      this._icon = val;
+      this._updateIcon();
+    }
+  }
+
+  var _size;
   @NgAttr('size')
-  String size;
+  String get size {
+    return this._size;
+  }
+  void set size(String val) {
+    if (val != this._size) {
+      this._size = val;
+      this._updateSize();
+    }
+  }
 
   String get iconUrl {
     return '/s/img/${this.icon}.png';
   }
 
-  void onAll(dom.ShadowRoot root) {
-    var imgElem = root.querySelector('img');
-    if (imgElem != null && this.icon != null) {
-      imgElem
-        ..style.width = this.size == null ? '80px' : this.size
-        ..attributes['src'] = this.iconUrl;
+  void _updateIcon() {
+    if (this.imgElem != null && this.icon != null) {
+      this.imgElem.attributes['src'] = this.iconUrl;
     }
+  }
+
+  void _updateSize() {
+    if (this.imgElem != null) {
+      this.imgElem.style.width = this.size == null ? '80px' : this.size;
+    }
+  }
+
+  void onAll(dom.ShadowRoot root) {
+    this.imgElem = root.querySelector('img');
+    this._updateIcon();
+    this._updateSize();
   }
 }
