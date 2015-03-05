f0ae2587949da6a6fc42efb2f8b9c4359e60ccfc
Zachary Huff <zach.huff.386@gmail.com>
2015-03-05 17:44:53 -0500

    Add accounts post handler

--- handlers/accounts.go
+++ handlers/accounts.go
@@ -1,8 +1,12 @@
 package handlers
 
 import (
+	"github.com/blckur/blckur/account"
+	"github.com/blckur/blckur/database"
+	"github.com/blckur/blckur/session"
 	"github.com/gin-gonic/gin"
 	"labix.org/v2/mgo/bson"
+	"math/rand"
 )
 
 type Account struct {
@@ -10,6 +14,33 @@ type Account struct {
 	UserId bson.ObjectId `json:"user_id" binding:"required"`
 	Type string `json:"type" binding:"required"`
 	Identity string `json:"identity" binding:"required"`
+}
+
+type AccountPostData struct {
+	Redirect string `json:"redirect" binding:"required"`
+}
+
+func accountsPost(c *gin.Context) {
+	acct := &AccountPostData{}
+	acctType := c.Params.ByName("type")
+	db := c.MustGet("db").(*database.Database)
+	sess := c.MustGet("session").(*session.Session)
+
+	if acctType == "twitter" {
+		url, err := account.AuthTwitter(db, sess.UserId)
+		if err != nil {
+			panic(err)
+		}
+		acct.Redirect = url
+	} else {
+		c.JSON(400, &ErrorData{
+			Error: "unknown_type",
+			Message: "Unknown account type",
+		})
+		return
+	}
+
+	c.JSON(200, acct)
 }
 
 func accountsGet(c *gin.Context) {
