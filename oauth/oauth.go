8b3fb5391b8457e87bc26b8b1f5767cfe8741609
Zachary Huff <zach.huff.386@gmail.com>
2015-03-06 20:00:00 -0500

    Add oauth

---
+++ oauth/oauth.go
@@ -0,0 +1,12 @@
+package oauth
+
+import (
+	"labix.org/v2/mgo/bson"
+)
+
+type Token struct {
+	Id string `bson:"_id"`
+	OauthSecret string `bson:"oauth_secret"`
+	Type string `bson:"type"`
+	UserId bson.ObjectId `bson:"user_id"`
+}
