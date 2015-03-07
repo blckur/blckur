610b6da5ca11646579dcec8fdd5339d57e18a849
Zachary Huff <zach.huff.386@gmail.com>
2015-03-07 02:09:49 -0500

    Add oauth2

---
+++ oauth/oauth2.go
@@ -0,0 +1,156 @@
+package oauth
+
+import (
+	"encoding/json"
+	"github.com/blckur/blckur/database"
+	"github.com/blckur/blckur/errortypes"
+	"github.com/blckur/blckur/utils"
+	"github.com/dropbox/godropbox/errors"
+	"golang.org/x/oauth2"
+	"labix.org/v2/mgo/bson"
+	"io/ioutil"
+	"time"
+	"net/http"
+)
+
+type Oauth2 struct {
+	Type string
+	ClientId string
+	ClientSecret string
+	CallbackUrl string
+	AuthUrl string
+	TokenUrl string
+	Scopes []string
+	conf *oauth2.Config
+}
+
+func (o *Oauth2) Init() {
+	o.conf = &oauth2.Config{
+		ClientID: o.ClientId,
+		ClientSecret: o.ClientSecret,
+		RedirectURL: o.CallbackUrl,
+		Scopes: o.Scopes,
+		Endpoint: oauth2.Endpoint{
+			AuthURL: o.AuthUrl,
+			TokenURL: o.TokenUrl,
+		},
+	}
+}
+
+func (o *Oauth2) Request(db *database.Database, userId bson.ObjectId) (
+		url string, err error) {
+	coll := db.Tokens()
+	state := utils.RandStr(32)
+
+	url = o.conf.AuthCodeURL(state, oauth2.AccessTypeOffline)
+	if err != nil {
+		err = &errortypes.UnknownError{
+			errors.Wrap(err, "account: Unknown oauth2 api error"),
+		}
+		return
+	}
+
+	tokn := &Token{
+		Id: state,
+		Type: o.Type,
+		UserId: userId,
+	}
+
+	err = coll.Insert(tokn)
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
+
+func (o *Oauth2) Authorize(db *database.Database, state string, code string) (
+		client *Oauth2Client, err error) {
+	coll := db.Tokens()
+	tokn := &Token{}
+
+	err = coll.FindOneId(state, tokn)
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	accessTokn, err := o.conf.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		err = &errortypes.UnknownError{
+			errors.Wrap(err, "account: Unknown google api error"),
+		}
+		return
+	}
+
+	client = &Oauth2Client{
+		UserId: tokn.UserId,
+		AccessToken: accessTokn.AccessToken,
+		RefreshToken: accessTokn.RefreshToken,
+		Expiry: accessTokn.Expiry,
+		client: o.conf.Client(oauth2.NoContext, accessTokn),
+		conf: o,
+	}
+
+	return
+}
+
+func (o *Oauth2) NewClient(userId bson.ObjectId, accessToken string,
+		refreshToken string, expiry time.Time) (client *Oauth2Client) {
+	tokn := &oauth2.Token{
+		AccessToken: accessToken,
+		TokenType: "Bearer",
+		RefreshToken: refreshToken,
+		Expiry: expiry,
+	}
+
+	client = &Oauth2Client{
+		UserId: userId,
+		AccessToken: accessToken,
+		RefreshToken: refreshToken,
+		Expiry: expiry,
+		client: o.conf.Client(oauth2.NoContext, tokn),
+		conf: o,
+	}
+
+	return
+}
+
+type Oauth2Client struct {
+	UserId bson.ObjectId
+	AccessToken string
+	RefreshToken string
+	Expiry time.Time
+	client *http.Client
+	conf *Oauth2
+}
+
+func (c *Oauth2Client) GetJson(url string, resp interface{}) (err error) {
+	httpResp, err := c.client.Get(url)
+	if err != nil {
+		err = &errortypes.UnknownError{
+			errors.Wrap(err, "oauth: Unknown oauth2 api error"),
+		}
+		return
+	}
+	defer httpResp.Body.Close()
+
+	body, err := ioutil.ReadAll(httpResp.Body)
+	if err != nil {
+		err = &errortypes.UnknownError{
+			errors.Wrap(err, "oauth: Unknown parse error"),
+		}
+		return
+	}
+
+	err = json.Unmarshal(body, resp)
+	if err != nil {
+		err = &errortypes.UnknownError{
+			errors.Wrap(err, "oauth: Unknown parse error"),
+		}
+		return
+	}
+
+	return
+}
