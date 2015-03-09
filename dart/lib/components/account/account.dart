29942b4291c5c212c217776bf9a9a748ed2ee9a0
Zachary Huff <zach.huff.386@gmail.com>
2015-03-08 22:37:59 -0400

    Add settings to account template

--- dart/lib/components/account/account.dart
+++ dart/lib/components/account/account.dart
@@ -4,12 +4,20 @@ import 'package:blckur/models/account.dart' as acct;
 
 import 'package:angular/angular.dart' show Component, NgOneWay;
 
+// Add attr new if new highlight account for init setup
+
 @Component(
   selector: 'x-account',
   templateUrl: 'packages/blckur/components/account/account.html',
   cssUrl: 'packages/blckur/components/account/account.css'
 )
 class AccountComp {
+  bool settings;
+
   @NgOneWay('model')
   acct.Account model;
+
+  void onSettings() {
+    this.settings = this.settings != true;
+  }
 }
