e6b1d5125385a0f1e746e2aa98afeac68d1963c0
Zachary Huff <zach.huff.386@gmail.com>
2015-03-11 17:49:12 -0400

    Add on enter on alert add

--- dart/lib/components/alert_add/alert_add.dart
+++ dart/lib/components/alert_add/alert_add.dart
@@ -4,6 +4,7 @@ import 'package:blckur/models/alert_type.dart' as alrt_typ;
 import 'package:blckur/models/account.dart' as acct;
 import 'package:blckur/collections/alert_types.dart' as alrt_typs;
 import 'package:blckur/loading.dart' as lodin;
+import 'package:blckur/enter_aware.dart' as ent_awr;
 import 'package:blckur/logger.dart' as logger;
 import 'package:blckur/alert.dart' as alrt;
 
@@ -14,7 +15,7 @@ import 'package:angular/angular.dart' show Component, NgTwoWay;
   templateUrl: 'packages/blckur/components/alert_add/alert_add.html',
   cssUrl: 'packages/blckur/components/alert_add/alert_add.css'
 )
-class AlertAddComp extends lodin.Loading {
+class AlertAddComp extends ent_awr.EnterAware with lodin.Loading {
   bool selected;
   dynamic typeValue;
   alrt_typ.AlertType typeModel;
@@ -49,7 +50,18 @@ class AlertAddComp extends lodin.Loading {
 
   void onClick(alrt_typ.AlertType model) {
     this.typeModel = model;
+
+    if (model.valueType == "" || model.valueType == null) {
+      this.onSave();
+    }
+
     this.selected = true;
+  }
+
+  void onEnter() {
+    if (this.selected) {
+      this.onSave();
+    }
   }
 
   void onSave() {
