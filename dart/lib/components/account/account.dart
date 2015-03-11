0eba9d59cfc1dcd4348a520a30cc962494ddd562
Zachary Huff <zach.huff.386@gmail.com>
2015-03-11 02:51:51 -0400

    Remove on save from account

--- dart/lib/components/account/account.dart
+++ dart/lib/components/account/account.dart
@@ -1,8 +1,6 @@
 library account_comp;
 
 import 'package:blckur/models/account.dart' as acct;
-import 'package:blckur/alert.dart' as alrt;
-import 'package:blckur/logger.dart' as logger;
 
 import 'package:angular/angular.dart' show Component, NgOneWay;
 
@@ -16,23 +14,11 @@ import 'package:angular/angular.dart' show Component, NgOneWay;
 class AccountComp {
   bool settings;
   bool addingAlert;
-  acct.Account settingsModel;
 
   @NgOneWay('model')
   acct.Account model;
 
   void onSettings() {
-    this.settingsModel = this.model.clone();
     this.settings = this.settings != true;
-  }
-
-  void onSave() {
-    this.settingsModel.save(['events']).then((_) {
-      this.settings = false;
-      this.model = this.settingsModel;
-    }).catchError((err) {
-      logger.severe('Failed to save account settings', err);
-      new alrt.Alert('Failed to save account settings');
-    });
   }
 }
