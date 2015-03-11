64ea0915f40d420c3c4afc1a695ffbb59986ad3d
Zachary Huff <zach.huff.386@gmail.com>
2015-03-11 18:54:59 -0400

    Add alerts to account

--- dart/lib/components/account/account.dart
+++ dart/lib/components/account/account.dart
@@ -1,6 +1,9 @@
 library account_comp;
 
 import 'package:blckur/models/account.dart' as acct;
+import 'package:blckur/loading.dart' as lodin;
+import 'package:blckur/logger.dart' as logger;
+import 'package:blckur/alert.dart' as alrt;
 
 import 'package:angular/angular.dart' show Component, NgOneWay;
 
@@ -11,7 +14,7 @@ import 'package:angular/angular.dart' show Component, NgOneWay;
   templateUrl: 'packages/blckur/components/account/account.html',
   cssUrl: 'packages/blckur/components/account/account.css'
 )
-class AccountComp {
+class AccountComp extends lodin.Loading {
   bool settings;
   bool addingAlert;
 
@@ -20,5 +23,19 @@ class AccountComp {
 
   void onSettings() {
     this.settings = this.settings != true;
+  }
+
+  void onAlertDel(Map<String, String> alert) {
+    if (!this.setLoading()) {
+      return;
+    }
+
+    this.model.alerts.remove(alert);
+    this.model.save(['alerts']).catchError((err) {
+      logger.severe('Failed to remove alert', err);
+      new alrt.Alert('Failed to remove alert');
+    }).whenComplete(() {
+      this.clearLoading();
+    });
   }
 }
