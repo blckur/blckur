a6d24198573a2feb557116550ff9c911f471bac0
Zachary Huff <zach.huff.386@gmail.com>
2015-03-14 15:12:03 -0400

    Delay loading bar

--- dart/lib/loading.dart
+++ dart/lib/loading.dart
@@ -1,17 +1,28 @@
 library loading;
 
+import 'dart:async' as async;
+
 class Loading {
+  var _loading;
   bool loading;
 
   bool setLoading() {
-    if (this.loading == true) {
+    if (this._loading == true) {
       return false;
     }
-    this.loading = true;
+    this._loading = true;
+
+    new async.Timer(const Duration(milliseconds: 250), () {
+      if (this._loading == true) {
+        this.loading = true;
+      }
+    });
+
     return true;
   }
 
   void clearLoading() {
+    this._loading = false;
     this.loading = false;
   }
 }
