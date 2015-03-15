930ad62b51974d5776be0d8ea9b650890d06c832
Zachary Huff <zach.huff.386@gmail.com>
2015-03-14 21:59:32 -0400

    Add escape to utils

--- utils/utils.go
+++ utils/utils.go
@@ -80,3 +80,23 @@ func InfToBool(obj interface{}) (val bool) {
 	}
 	return
 }
+
+func isEscapable(b byte) bool {
+	return !('A' <= b && b <= 'Z' || 'a' <= b && b <= 'z' ||
+		'0' <= b && b <= '9' || b == '-' || b == '.' || b == '_' || b == '~')
+}
+
+func Escape(s string) string {
+	t := make([]byte, 0, 3 * len(s))
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if isEscapable(c) {
+			t = append(t, '%')
+			t = append(t, "0123456789ABCDEF"[c >> 4])
+			t = append(t, "0123456789ABCDEF"[c & 15])
+		} else {
+			t = append(t, s[i])
+		}
+	}
+	return string(t)
+}
