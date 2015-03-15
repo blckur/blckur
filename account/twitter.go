5b2369825b4a763dd30ab3bcc64cae9e3fc281e3
Zachary Huff <zach.huff.386@gmail.com>
2015-03-15 19:10:15 -0400

    Add stream to twitter

--- account/twitter.go
+++ account/twitter.go
@@ -6,8 +6,14 @@ import (
 	"github.com/blckur/blckur/requires"
 	"github.com/blckur/blckur/messenger"
 	"github.com/blckur/blckur/oauth"
+	"github.com/blckur/blckur/notification"
 	"github.com/ChimeraCoder/anaconda"
 	"labix.org/v2/mgo/bson"
+	"crypto/md5"
+	"net/url"
+	"time"
+	"strconv"
+	"encoding/hex"
 )
 
 var (
@@ -45,6 +51,107 @@ func (t *Twitter) Update() (err error) {
 
 	t.Identity = "@" + data.ScreenName
 	t.IdentityId = data.IdStr
+
+	return
+}
+
+func (t *Twitter) Parse(db *database.Database, evtInf interface{}) (
+		notf *notification.Notification, err error) {
+	var timestamp string
+
+	if evt, ok := evtInf.(anaconda.Tweet); ok {
+		_ = evt
+		return
+	} else if evt, ok := evtInf.(anaconda.EventTweet); ok {
+		if evt.Target.IdStr != t.IdentityId ||
+				evt.Source.IdStr == t.IdentityId {
+			return
+		}
+
+		var subject string
+		switch (evt.Event.Event) {
+		case "favorite":
+			subject = "Tweet favorited by "
+		case "unfavorite":
+			subject = "Tweet unfavorited by "
+		default:
+				return
+		}
+
+		origin := "@" + evt.Source.ScreenName
+		timestamp = evt.CreatedAt
+		subject += origin
+
+		notf = &notification.Notification{
+			UserId: t.UserId,
+			AccountId: t.Id,
+			Type: evt.Event.Event,
+			Resource: evt.TargetObject.IdStr,
+			Origin: origin,
+			Subject: subject,
+			Body: evt.TargetObject.Text,
+		}
+	} else if evt, ok := evtInf.(anaconda.Event); ok {
+		if evt.Target.IdStr != t.IdentityId || evt.Event != "follow" {
+			return
+		}
+
+		origin := "@" + evt.Source.ScreenName
+		timestamp = evt.CreatedAt
+		notf = &notification.Notification{
+			UserId: t.UserId,
+			AccountId: t.Id,
+			Type: evt.Event,
+			Resource: evt.Target.IdStr,
+			Origin: origin,
+			Subject: "New follower " + origin,
+		}
+	} else if evt, ok := evtInf.(anaconda.StatusDeletionNotice); ok {
+		coll := db.Notifications()
+
+		err = coll.Remove(bson.M{
+			"account_id": t.Id,
+			"resource": evt.IdStr,
+		})
+		if err != nil {
+			err = database.ParseError(err)
+			return
+		}
+
+		return
+	} else {
+		return
+	}
+
+	notf.Timestamp, err = time.Parse("Mon Jan 02 15:04:05 -0700 2006",
+		timestamp)
+	if err != nil {
+		notf = nil
+		err = nil
+	}
+
+	notf.RemoteId = hashEvent(t.Id.Hex(), notf.Timestamp)
+
+	return
+}
+
+func (t *Twitter) Stream(db *database.Database) (err error) {
+	client := t.NewApiClient()
+
+	streamVals := url.Values{}
+	streamVals.Add("with", "user")
+	streamVals.Add("replies", "all")
+
+	stream := client.UserStream(streamVals)
+
+	for obj := range stream.C {
+		notf, err := t.Parse(db, obj)
+		if err != nil {
+			panic(err)
+		}
+
+		_ = notf
+	}
 
 	return
 }
