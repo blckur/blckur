9fa7290d202a270626d6b5850b27394c85bb531a
Zachary Huff <zach.huff.386@gmail.com>
2015-03-16 19:08:57 -0400

    Add beanstalkd node

---
+++ nodes/beanstalkd.go
@@ -0,0 +1,93 @@
+package nodes
+
+import (
+	"github.com/blckur/blckur/constants"
+	"github.com/blckur/blckur/database"
+	"github.com/blckur/blckur/logger"
+	"github.com/Sirupsen/logrus"
+	"github.com/blckur/blckur/utils"
+	"labix.org/v2/mgo/bson"
+	"os/exec"
+	"time"
+	"strconv"
+)
+
+type BeanstalkdNode struct {
+	Id string
+	Host string
+	Port int
+}
+
+func (b *BeanstalkdNode) Start() {
+	portInt := b.Port
+	if portInt == 0 {
+		portInt = 11300
+	}
+	port := strconv.Itoa(portInt)
+
+	args := []string{"-p", port}
+	if b.Host != "" {
+		args = append(args, "-l", b.Host)
+	}
+
+	address, err := utils.GetLocalAddress()
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error": err,
+		}).Error("beanstalkd: Failed to get ip")
+		panic(err)
+	}
+	address = address + ":" + port
+
+	var cmd *exec.Cmd
+	for {
+		if cmd != nil {
+			cmd.Process.Kill()
+			cmd.Wait()
+			time.Sleep(constants.RETRY_DELAY)
+		}
+
+		cmd = exec.Command("./beanstalkd2", args...) // TODO
+		cmd.Stdout = logger.NewErrorWriter()
+		cmd.Stderr = logger.NewErrorWriter()
+
+		var cmdErr error
+		go func() {
+			cmdErr = cmd.Run()
+		}()
+
+		db := database.GetDatabase()
+		coll := db.Nodes()
+
+		delay := false
+		for {
+			if delay {
+				time.Sleep(constants.RETRY_DELAY)
+			} else {
+				delay = true
+			}
+
+			if cmdErr != nil {
+				logrus.WithFields(logrus.Fields{
+					"error": cmd.ProcessState.String(),
+				}).Error("beanstalkd: Unexpected exit")
+				break
+			}
+
+			_, err := coll.UpsertId(b.Id, &bson.M{
+				"type": "beanstalkd",
+				"address": address,
+				"timestamp": time.Now(),
+			})
+			if err != nil {
+				err = database.ParseError(err)
+				logrus.WithFields(logrus.Fields{
+					"error": err,
+				}).Error("beanstalkd: Database upsert")
+				continue
+			}
+
+			time.Sleep(5 * time.Second)
+		}
+	}
+}
