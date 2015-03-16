a5b2f8e373bd04bc5f692b4a79db6b68e2703bb3
Zachary Huff <zach.huff.386@gmail.com>
2015-03-16 13:10:16 -0400

    Add update to twitter stream

--- streams/twitter.go
+++ streams/twitter.go
@@ -3,6 +3,8 @@ package streams
 import (
 	"github.com/blckur/blckur/account"
 	"github.com/blckur/blckur/database"
+	"github.com/blckur/blckur/settings"
+	"time"
 )
 
 func (s *Stream) startTwitter() (err error) {
@@ -14,8 +16,35 @@ func (s *Stream) startTwitter() (err error) {
 	acct := account.Twitter(*acctTyp)
 	stream := acct.Stream(db)
 
+	err = s.initialize()
+	if err != nil {
+		return
+	}
+
 	go func() {
 		stream.Start()
+	}()
+
+	go func() {
+		lastUpdate := time.Now()
+		for {
+			time.Sleep(time.Duration(
+				settings.Stream.RefreshRate) * time.Second)
+			stop, err := s.Update()
+			if err != nil {
+				panic(err)
+			} else if stop {
+				stream.Stop()
+				return
+			} else {
+				lastUpdate = time.Now()
+			}
+
+			if time.Since(lastUpdate) > 2 * time.Minute {
+				stream.Stop()
+				return
+			}
+		}
 	}()
 
 	return
