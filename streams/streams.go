233fe2cc7cd21bb7a9de092c6266adcb3474bfa4
Zachary Huff <zach.huff.386@gmail.com>
2015-03-16 03:21:08 -0400

    Add new stream

--- streams/streams.go
+++ streams/streams.go
@@ -1,6 +1,8 @@
 package streams
 
 import (
+	"github.com/blckur/blckur/database"
+	"labix.org/v2/mgo"
 	"labix.org/v2/mgo/bson"
 	"time"
 )
@@ -10,4 +12,17 @@ type Stream struct {
 	RunnerId bson.ObjectId `bson:"runner_id"`
 	Type string `bson:"type"`
 	Timestamp time.Time `bson:"timestamp"`
+	db *database.Database
+}
+
+func NewStream(acctId bson.ObjectId, typ string) (stream *Stream) {
+	db := database.GetDatabase()
+
+	stream = &Stream{
+		Id: acctId,
+		Type: typ,
+		db: db,
+	}
+
+	return
 }
