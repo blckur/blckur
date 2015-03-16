21f60107d847b430c9de2c0df70f7325cd7ba18c
Zachary Huff <zach.huff.386@gmail.com>
2015-03-16 19:08:20 -0400

    Add server cmd

---
+++ cmd/server.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"github.com/blckur/blckur/database"
+	"github.com/blckur/blckur/handlers"
+	"github.com/blckur/blckur/logger"
+	"github.com/blckur/blckur/session"
+	"github.com/blckur/blckur/settings"
+	"github.com/blckur/blckur/utils"
+	"github.com/blckur/blckur/messenger"
+	"github.com/blckur/blckur/account"
+	"github.com/Sirupsen/logrus"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+	"os"
+)
+
+func Server() {
+	utils.SeedRand()
+
+	logger.Init()
+	database.Init()
+	settings.Init()
+
+	host := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+	addr := host + ":" + port
+
+	account.Init()
+	messenger.Init()
+	session.Init()
+
+	router := gin.Default()
+	handlers.Register(router)
+
+	server := &http.Server{
+		Addr: addr,
+		Handler: router,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		MaxHeaderBytes: 4096,
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"address": addr,
+	}).Info("main: Starting server")
+
+	server.ListenAndServe()
+}
