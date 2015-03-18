a48daca7af6627e0f9066f57f770f7bfe70f9dc9
Zachary Huff <zach.huff.386@gmail.com>
2015-03-17 23:23:42 -0400

    Change default redis timeout

--- settings/settings.go
+++ settings/settings.go
@@ -77,7 +77,7 @@ type stream struct {
 type redis struct {
 	Id string `bson:"_id"`
 	Consistency int `bson:"consistency" default:"2"`
-	TimeoutMilli int `bson:"timeout_milli" default:"2000"`
+	TimeoutMilli int `bson:"timeout_milli" default:"2500"`
 	MaxIdle int `bson:"max_idle" default:"3"`
 	MaxActive int `bson:"max_active" default:"0"`
 	IdleTimeout int `bson:"idle_timeout" default:"300"`
