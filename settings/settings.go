6624a7eb295d8d0597cad4f4355abd055a95f3f3
Zachary Huff <zach.huff.386@gmail.com>
2015-03-17 23:22:10 -0400

    Add timeout milli to settings

--- settings/settings.go
+++ settings/settings.go
@@ -77,6 +77,7 @@ type stream struct {
 type redis struct {
 	Id string `bson:"_id"`
 	Consistency int `bson:"consistency" default:"2"`
+	TimeoutMilli int `bson:"timeout_milli" default:"2000"`
 	MaxIdle int `bson:"max_idle" default:"3"`
 	MaxActive int `bson:"max_active" default:"0"`
 	IdleTimeout int `bson:"idle_timeout" default:"300"`
