ba648bd6aaf1f1ef6139b274a08240b03a2ead12
Zachary Huff <zach.huff.386@gmail.com>
2015-03-17 23:01:30 -0400

    Add node

---
+++ nodes/nodes.go
@@ -0,0 +1,12 @@
+package nodes
+
+import (
+	"time"
+)
+
+type Node struct {
+	Id string `bson:"_id"`
+	Type string `bson:"type"`
+	Timestamp time.Time `bson:"timestamp"`
+	Address string `bson:"address"`
+}
