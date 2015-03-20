138065c63ed314ee9e3d4a06283b71a32a04ec42
Zachary Huff <zach.huff.386@gmail.com>
2015-03-20 16:34:58 -0400

    Remove init from tasks

--- tasks/tasks.go
+++ tasks/tasks.go
@@ -13,6 +13,17 @@ type Task interface {
 }
 
 func register(hour int, min int, task Task) {
+	if len(tasksRegistry) == 0 {
+		tasksRegistry = make([][][]Task, 25)
+
+		for i := 0; i < 25; i++ {
+			tasksRegistry[i] = make([][]Task, 61)
+			for j := 0; j < 61; j++ {
+				tasksRegistry[i][j] = []Task{}
+			}
+		}
+	}
+
 	tasksRegistry[hour + 1][min + 1] = append(
 		tasksRegistry[hour + 1][min + 1],
 		task)
@@ -38,15 +49,4 @@ func GetTasks(tme time.Time) (tasks []Task) {
 	}
 
 	return
-}
-
-func init() {
-	tasksRegistry = make([][][]Task, 25)
-
-	for i := 0; i < 25; i++ {
-		tasksRegistry[i] = make([][]Task, 61)
-		for j := 0; j < 61; j++ {
-			tasksRegistry[i][j] = []Task{}
-		}
-	}
 }
