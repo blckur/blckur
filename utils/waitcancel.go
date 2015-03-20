e59ee8e74a7e184c6fc48ba6f7cb2c2838d6089f
Zachary Huff <zach.huff.386@gmail.com>
2015-03-20 01:32:00 -0400

    Improve waitcancel

--- utils/waitcancel.go
+++ utils/waitcancel.go
@@ -1,35 +1,40 @@
 package utils
 
+import (
+	"sync/atomic"
+)
+
+// Warning: Add() can not be called after Wait()
 type WaitCancel struct {
-	count int
-	wait chan bool
+	count int32
+	wait chan int
 }
 
 func (w *WaitCancel) Add(n int) {
 	if w.wait == nil {
-		w.wait = make(chan bool)
+		w.wait = make(chan int)
 	}
-	w.count += n
+	atomic.AddInt32(&w.count, int32(n))
 }
 
 func (w *WaitCancel) Done() {
-	w.wait <- false
+	val := atomic.AddInt32(&w.count, -1)
+	if val == -1 {
+		w.wait <- 1
+	}
 }
 
 func (w *WaitCancel) Cancel() {
-	w.wait <- true
+	val := atomic.SwapInt32(&w.count, -1073741824)
+	if val >= 0 {
+		w.wait <- 1
+	}
 }
 
 func (w *WaitCancel) Wait() {
-	done := 0
-	for {
-		if <-w.wait {
-			return
-		} else {
-			done += 1
-			if done >= w.count {
-				return
-			}
-		}
+	val := atomic.AddInt32(&w.count, -1)
+	if val == -1 {
+		return
 	}
+	<-w.wait
 }
