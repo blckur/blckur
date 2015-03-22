4afc7e0489d7596a140a30fde0afb550e3979798
Zachary Huff <zach.huff.386@gmail.com>
2015-03-22 15:08:31 -0400

    Use get service options in cache

--- cmd/cache.go
+++ cmd/cache.go
@@ -6,8 +6,6 @@ import (
 	"github.com/blckur/blckur/settings"
 	"github.com/blckur/blckur/logger"
 	"github.com/blckur/blckur/messenger"
-	"os"
-	"strconv"
 )
 
 func Cache() {
@@ -15,26 +13,12 @@ func Cache() {
 	database.Init()
 	settings.Init()
 	messenger.Init()
-
-	id := os.Getenv("ID")
-	if id == "" {
-		panic("Node ID is required")
-	}
-	host := os.Getenv("HOST")
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "0"
-	}
-
-	portInt, err := strconv.Atoi(port)
-	if err != nil {
-		panic(err)
-	}
+	opts := GetServiceOptions()
 
 	node := nodes.CacheNode{
-		Id: id,
-		Host: host,
-		Port: portInt,
+		Id: opts.Id,
+		Host: opts.Host,
+		Port: opts.Port,
 	}
 
 	node.Start()
