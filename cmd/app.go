1f0e1964288b73147ea49fd0dca67fe7fcd549f3
Zachary Huff <zach.huff.386@gmail.com>
2015-03-23 01:38:36 -0400

    Update app cmd

--- cmd/app.go
+++ cmd/app.go
@@ -3,50 +3,29 @@ package cmd
 import (
 	"github.com/blckur/blckur/database"
 	"github.com/blckur/blckur/cache"
-	"github.com/blckur/blckur/handlers"
 	"github.com/blckur/blckur/logger"
 	"github.com/blckur/blckur/session"
 	"github.com/blckur/blckur/settings"
 	"github.com/blckur/blckur/messenger"
 	"github.com/blckur/blckur/account"
-	"github.com/Sirupsen/logrus"
-	"github.com/gin-gonic/gin"
-	"net/http"
-	"time"
-	"os"
+	"github.com/blckur/blckur/nodes"
 )
 
 func App() {
 	logger.Init()
 	database.Init()
 	settings.Init()
-
-	host := os.Getenv("HOST")
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "80"
-	}
-	addr := host + ":" + port
-
 	account.Init()
 	cache.Init()
 	messenger.Init()
 	session.Init()
+	opts := GetServiceOptions()
 
-	router := gin.Default()
-	handlers.Register(router)
-
-	server := &http.Server{
-		Addr: addr,
-		Handler: router,
-		ReadTimeout: 10 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		MaxHeaderBytes: 4096,
+	node := nodes.AppNode{
+		Id: opts.Id,
+		Host: opts.Host,
+		Port: opts.Port,
 	}
 
-	logrus.WithFields(logrus.Fields{
-		"address": addr,
-	}).Info("nodes.app: Starting app node")
-
-	server.ListenAndServe()
+	node.Start()
 }
