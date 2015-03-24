be44c65ebd8a6bcf6216bf9f3861aed3299e4b55
Zachary Huff <zach.huff.386@gmail.com>
2015-03-24 19:04:47 -0400

    Imrpove requires

--- requires/requires.go
+++ requires/requires.go
@@ -2,33 +2,72 @@ package requires
 
 import (
 	"github.com/dropbox/godropbox/container/set"
-	"github.com/dropbox/godropbox/errors"
+	"container/list"
 )
 
 var (
-	loaded = set.NewSet()
+	modules = list.New()
 )
 
-func Before(name string) {
-	if loaded.Contains(name) {
-		err := &InitError{
-			errors.New("settings: Init before error"),
-		}
-		panic(err)
+type Module struct {
+	name string
+	before set.Set
+	after set.Set
+	Handler func()
+}
+
+func (m *Module) Before(name string) {
+	m.before.Add(name)
+}
+
+func (m *Module) After(name string) {
+	m.after.Add(name)
+}
+
+func New(name string) (module *Module) {
+	module = &Module{
+		name: name,
+		before: set.NewSet(),
+		after: set.NewSet(),
 	}
+	modules.PushBack(module)
 	return
 }
 
-func After(name string) {
-	if !loaded.Contains(name) {
-		err := &InitError{
-			errors.New("settings: Init after error"),
-		}
-		panic(err)
-	}
-	return
-}
+func Init() {
+	Loop:
+	for {
+		i := modules.Front()
+		for i != nil {
+			module := i.Value.(*Module)
 
-func Register(name string) {
-	loaded.Add(name)
+			j := i.Prev()
+			for j != nil {
+				if module.before.Contains(j.Value.(*Module).name) {
+					modules.MoveBefore(i, j)
+					continue Loop
+				}
+				j = j.Prev()
+			}
+
+			j = i.Next()
+			for j != nil {
+				if module.after.Contains(j.Value.(*Module).name) {
+					modules.MoveAfter(i, j)
+					continue Loop
+				}
+				j = j.Next()
+			}
+
+			i = i.Next()
+		}
+
+		break
+	}
+
+	i := modules.Front()
+	for i != nil {
+		i.Value.(*Module).Handler()
+		i = i.Next()
+	}
 }
