6f3883879c26c998dff8f356ebfcb3d4dafa758b
Zachary Huff <zach.huff.386@gmail.com>
2015-03-24 19:25:19 -0400

    Update app init

--- cmd/app.go
+++ cmd/app.go
@@ -1,24 +1,14 @@
 package cmd
 
 import (
-	"github.com/blckur/blckur/database"
-	"github.com/blckur/blckur/cache"
-	"github.com/blckur/blckur/logger"
-	"github.com/blckur/blckur/session"
-	"github.com/blckur/blckur/settings"
-	"github.com/blckur/blckur/messenger"
-	"github.com/blckur/blckur/account"
 	"github.com/blckur/blckur/nodes"
+	"github.com/blckur/blckur/requires"
 )
 
 func App() {
-	logger.Init()
-	database.Init()
-	settings.Init()
-	account.Init()
-	cache.Init()
-	messenger.Init()
-	session.Init()
+	requires.Init()
+
+
 	opts := GetServiceOptions()
 
 	node := nodes.AppNode{
