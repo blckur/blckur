07078f8cba52e9273076bff7a178630a756e52c1
Zachary Huff <zach.huff.386@gmail.com>
2015-03-24 19:13:49 -0400

    Make sort alerts private

--- account/alert_sort.go
+++ account/alert_sort.go
@@ -4,26 +4,26 @@ import (
 	"sort"
 )
 
-type AlertsSort struct {
+type alertsSort struct {
 	Alerts []*Alert
 }
 
-func (a *AlertsSort) Len() (n int) {
+func (a *alertsSort) Len() (n int) {
 	n = len(a.Alerts)
 	return
 }
 
-func (a *AlertsSort) Less(i int, j int) (x bool) {
+func (a *alertsSort) Less(i int, j int) (x bool) {
 	x = a.Alerts[i].Label < a.Alerts[j].Label
 	return
 }
 
-func (a *AlertsSort) Swap(i int, j int) {
+func (a *alertsSort) Swap(i int, j int) {
 	a.Alerts[i], a.Alerts[j] = a.Alerts[j], a.Alerts[i]
 }
 
-func (a *Account) SortAlerts() {
-	srt := &AlertsSort{
+func (a *Account) sortAlerts() {
+	srt := &alertsSort{
 		Alerts: a.Alerts,
 	}
 	sort.Sort(srt)
