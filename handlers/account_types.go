bcf59d4cf10ae4fd5d9188fde51033084a65ddb9
Zachary Huff <zach.huff.386@gmail.com>
2015-03-25 20:59:12 -0400

    Update account types handler

--- handlers/account_types.go
+++ handlers/account_types.go
@@ -1,50 +1,10 @@
 package handlers
 
 import (
+	"github.com/blckur/blckur/account"
 	"github.com/gin-gonic/gin"
 )
 
-var (
-	types = []accountType{
-		accountType{
-			Id: 1,
-			Label: "Twitter",
-			Type: "twitter",
-		},
-		accountType{
-			Id: 2,
-			Label: "GitHub",
-			Type: "github",
-		},
-		accountType{
-			Id: 3,
-			Label: "Gmail",
-			Type: "gmail",
-		},
-		accountType{
-			Id: 4,
-			Label: "HipChat",
-			Type: "hipchat",
-		},
-		accountType{
-			Id: 5,
-			Label: "Bitly",
-			Type: "bitly",
-		},
-		accountType{
-			Id: 6,
-			Label: "DigitalOcean",
-			Type: "digitalocean",
-		},
-	}
-)
-
-type accountType struct {
-	Id int `json:"id"`
-	Label string `json:"label"`
-	Type string `json:"type"`
-}
-
 func accountsTypesGet(c *gin.Context) {
-	c.JSON(200, types)
+	c.JSON(200, account.GetAccountTypes())
 }
