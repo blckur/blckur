f6e2b437fa64330ef3bf7b7403f1f9831f4e1668
Zachary Huff <zach.huff.386@gmail.com>
2015-03-26 19:57:34 -0400

    Remove account alerts from alert

--- account/alert.go
+++ account/alert.go
@@ -4,51 +4,6 @@ import (
 	"labix.org/v2/mgo/bson"
 )
 
-var (
-	AlertTypes = map[string][]AlertType{
-		"gmail": []AlertType{
-			AlertType{
-				Id: 1,
-				Label: "All new messages",
-				Type: "all",
-			},
-			AlertType{
-				Id: 2,
-				Label: "New messages matching sender",
-				Type: "from",
-				ValueType: "input",
-				ValueLabel: "Enter complete or partial email address " +
-				"of sender to match",
-				ValueHolder: "Email address",
-			},
-			AlertType{
-				Id: 3,
-				Label: "New messages matching subject",
-				Type: "subject",
-				ValueType: "input",
-				ValueLabel: "Enter search term to match in email subject",
-				ValueHolder: "Search term",
-			},
-			AlertType{
-				Id: 4,
-				Label: "New messages matching message body",
-				Type: "body",
-				ValueType: "input",
-				ValueLabel: "Enter search term to match in email body",
-				ValueHolder: "Search term",
-			},
-		},
-	}
-	AlertLabels = map[string]map[string]string{
-		"gmail": map[string]string{
-			"all": "All messages",
-			"from": "Messages matching sender",
-			"subject": "Messages matching subject",
-			"body": "Messages matching message body",
-		},
-	}
-)
-
 type AlertType struct {
 	Id int `json:"id"`
 	Label string `json:"label"`
