2163aabf3f4447c6826e1f06e20d64aa0bdd31d3
Zachary Huff <zach.huff.386@gmail.com>
2015-03-26 22:28:58 -0400

    Add alert types to digitalocean

--- account/digitalocean.go
+++ account/digitalocean.go
@@ -18,30 +18,8 @@ func init() {
 	DigitalOceanAuth{}, DigitalOceanClient{},
 	[]*AlertType{
 		&AlertType{
-			Label: "All new messages",
+			Label: "All new events",
 			Type: "all",
-		},
-		&AlertType{
-			Label: "New messages matching sender",
-			Type: "from",
-			ValueType: "input",
-			ValueLabel: "Enter complete or partial email address " +
-			"of sender to match",
-			ValueHolder: "Email address",
-		},
-		&AlertType{
-			Label: "New messages matching subject",
-			Type: "subject",
-			ValueType: "input",
-			ValueLabel: "Enter search term to match in email subject",
-			ValueHolder: "Search term",
-		},
-		&AlertType{
-			Label: "New messages matching message body",
-			Type: "body",
-			ValueType: "input",
-			ValueLabel: "Enter search term to match in email body",
-			ValueHolder: "Search term",
 		},
 	}, func() {
 		messenger.Register("settings", "digitalocean",
