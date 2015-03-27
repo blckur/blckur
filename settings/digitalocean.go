fd5e26b4cbb518656113d73790dcd02c01a42da5
Zachary Huff <zach.huff.386@gmail.com>
2015-03-26 22:26:58 -0400

    Change digitalocean settings

--- settings/digitalocean.go
+++ settings/digitalocean.go
@@ -12,5 +12,5 @@ type digitalocean struct {
 	Id string `bson:"_id"`
 	ClientId string `bson:"client_id"`
 	ClientSecret string  `bson:"client_secret"`
-	MaxMsg int `bson:"max_msg" default:"100"`
+	MaxEvents int `bson:"max_events" default:"100"`
 }
