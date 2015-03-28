63fac53a7a4bb5410a38922e22eda3d109f6ce3e
Zachary Huff <zach.huff.386@gmail.com>
2015-03-28 02:26:00 -0400

    Add modeswitch

--- dart/lib/injectables/injectables.dart
+++ dart/lib/injectables/injectables.dart
@@ -2,11 +2,14 @@ library injectables;
 
 import 'package:blckur/injectables/loading.dart' as loading;
 export 'package:blckur/injectables/loading.dart' show Loading;
+import 'package:blckur/injectables/modeswitch.dart' as modeswitch;
+export 'package:blckur/injectables/modeswitch.dart' show ModeSwitch;
 
 import 'package:angular/angular.dart' as ng;
 
 class InjectablesMod extends ng.Module {
   InjectablesMod() {
     this.bind(loading.Loading);
+    this.bind(modeswitch.ModeSwitch);
   }
 }
