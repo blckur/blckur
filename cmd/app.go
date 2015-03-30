87b09772f4c28597ad73eeb9ca5ce63ea4c67799
Zachary Huff <zach.huff.386@gmail.com>
2015-03-30 03:08:36 -0400

    Add source to app cmd

--- cmd/app.go
+++ cmd/app.go
@@ -2,16 +2,23 @@ package cmd
 
 import (
 	"github.com/blckur/blckur/nodes"
+	"os"
 )
 
 // Starts web server node with JSON api
 func App() {
 	opts := getServiceOptions()
 
+	source := os.Getenv("SOURCE")
+	if source != "" {
+		panic("Missing source")
+	}
+
 	node := nodes.AppNode{
 		Id: opts.Id,
 		Host: opts.Host,
 		Port: opts.Port,
+		Source: source,
 	}
 
 	node.Start()
