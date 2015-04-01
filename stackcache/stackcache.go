b81c0d1ff71e60e680b6d59cc55ac1f57b8595d2
Zachary Huff <zach.huff.386@gmail.com>
2015-03-31 20:01:59 -0400

    Fix return in stachcache

--- stackcache/stackcache.go
+++ stackcache/stackcache.go
@@ -31,12 +31,11 @@ func (s *IdStackCache) Contains(elem bson.ObjectId) bool {
 	return s.data.Contains(elem)
 }
 
-func NewIdStackCache(limit int) (stck *IdStackCache) {
-	stck = &IdStackCache{
+func NewIdStackCache(limit int) *IdStackCache {
+	return &IdStackCache{
 		queue: make(chan bson.ObjectId, limit),
 		data: set.NewSet(),
 		limit: limit,
 		mutex: &sync.Mutex{},
 	}
-	return
 }
