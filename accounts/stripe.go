737a773a8d6b195e182ec232cf3aeb4424be8ff8
Zachary Huff <zach.huff.386@gmail.com>
2015-04-06 19:56:31 -0400

    Add stripe account

---
+++ accounts/stripe.go
@@ -0,0 +1,118 @@
+package accounts
+
+import (
+	"github.com/blckur/blckur/account"
+	"github.com/blckur/blckur/database"
+	"github.com/blckur/blckur/messenger"
+	"github.com/blckur/blckur/oauth"
+	"github.com/blckur/blckur/settings"
+	"labix.org/v2/mgo/bson"
+)
+
+var (
+	stripeConf *oauth.Oauth2
+)
+
+func init() {
+	account.Register("stripe", "Stripe", OAUTH2,
+		StripeAuth{}, StripeClient{},
+		[]*account.FilterType{
+			&account.FilterType{
+				Label: "All new events",
+				Type:  "all",
+			},
+		}, func() {
+			messenger.Register("settings", "stripe",
+				func(_ *messenger.Message) {
+					updateStripe()
+				})
+			updateStripe()
+		})
+}
+
+type StripeClient struct {
+	acct *account.Account
+}
+
+func (s *StripeClient) SetAccount(acct *account.Account) {
+	s.acct = acct
+}
+
+func (s *StripeClient) Update(db *database.Database) (err error) {
+	client := stripeConf.NewClient(g.acct)
+
+	data := struct {
+		Login string `json:"login"`
+	}{}
+
+	err = client.GetJson("", &data)
+	if err != nil {
+		return
+	}
+
+	s.acct.Identity = data.Login
+
+	return
+}
+
+func (s *StripeClient) Sync(db *database.Database) (err error) {
+	return
+}
+
+type StripeAuth struct{}
+
+func (s *StripeAuth) Request(db *database.Database,
+	userId bson.ObjectId) (url string, err error) {
+
+	url, err = stripeConf.Request(db, userId)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (s *StripeAuth) Authorize(db *database.Database, state string,
+	code string) (acct *account.Account, err error) {
+
+	coll := db.Accounts()
+
+	auth, err := stripeConf.Authorize(db, state, code)
+	if err != nil {
+		return
+	}
+
+	client, err := auth.Account.GetClient()
+	if err != nil {
+		return
+	}
+
+	err = client.Update(db)
+	if err != nil {
+		return
+	}
+
+	err = coll.Insert(auth.Account)
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
+
+func updateStripe() {
+	stripeConf = &oauth.Oauth2{
+		Type:         "github",
+		ClientId:     settings.Stripe.ClientId,
+		ClientSecret: settings.Stripe.ClientSecret,
+		CallbackUrl:  settings.System.Domain + "/callback/stripe",
+		AuthUrl:      "https://connect.stripe.com/oauth/authorize",
+		TokenUrl:     "https://connect.stripe.com/oauth/token",
+		Scopes: []string{
+			"repo:status",
+			"notifications",
+		},
+	}
+	stripeConf.Config()
+}
