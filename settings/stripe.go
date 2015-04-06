c7dbbfa1fb87a2c566f1d146dae2a4b9c1fbfa9c
Zachary Huff <zach.huff.386@gmail.com>
2015-04-06 19:53:27 -0400

    Add stripe to settings

---
+++ settings/stripe.go
@@ -0,0 +1,15 @@
+package settings
+
+var Stripe = &stripe{
+	Id: "stripe",
+}
+
+func init() {
+	register("stripe", Stripe)
+}
+
+type stripe struct {
+	Id           string `bson:"_id"`
+	ClientId     string `bson:"client_id"`
+	ClientSecret string `bson:"client_secret"`
+}
