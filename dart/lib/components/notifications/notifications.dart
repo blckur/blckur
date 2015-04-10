d056e7d81789bf422167b28b9e6eef44e8c95f00
Zachary Huff <zach.huff.386@gmail.com>
2015-04-10 17:57:43 -0400

    Handle notf update event

--- dart/lib/components/notifications/notifications.dart
+++ dart/lib/components/notifications/notifications.dart
@@ -7,13 +7,14 @@ import 'package:blckur/logger.dart' as logger;
 import 'package:blckur/alert.dart' as alert;
 
 import 'package:angular/angular.dart' show Component, NgOneWay;
+import 'package:angular/angular.dart' as ng;
 
 @Component(
   selector: 'x-notifications',
   templateUrl: 'packages/blckur/components/notifications/notifications.html',
   cssUrl: 'packages/blckur/components/notifications/notifications.css'
 )
-class NotificationsComp {
+class NotificationsComp implements ng.ScopeAware {
   models.Notification model;
   collections.Notifications notifications;
   utils.Loading loading;
@@ -23,6 +24,12 @@ class NotificationsComp {
     this.notifications = new collections.Notifications();
     this.loading = new utils.Loading();
     this.update();
+  }
+
+  void set scope(ng.Scope scope) {
+    scope.on('notf_update').listen((_) {
+      this.update();
+    });
   }
 
   void update() {
