b2882d8524f3cc0dc9a7d0abb955d6425d2c706e
Zachary Huff <zach.huff.386@gmail.com>
2015-04-11 19:59:45 -0400

    Add notifcation put

--- handlers/notifications.go
+++ handlers/notifications.go
@@ -5,7 +5,12 @@ import (
 	"github.com/blckur/blckur/notification"
 	"github.com/blckur/blckur/session"
 	"github.com/gin-gonic/gin"
+	"labix.org/v2/mgo/bson"
 )
+
+type notificationData struct {
+	Read bool `json:"read"`
+}
 
 func notificationGet(c *gin.Context) {
 	db := c.MustGet("db").(*database.Database)
@@ -18,4 +23,23 @@ func notificationGet(c *gin.Context) {
 	}
 
 	c.JSON(200, accts)
+}
+
+func notificationPut(c *gin.Context) {
+	db := c.MustGet("db").(*database.Database)
+	sess := c.MustGet("session").(*session.Session)
+	notfId := c.Params.ByName("id").(bson.ObjectId)
+	data := &notificationData{}
+
+	c.Bind(data)
+
+	notf, err := notification.GetNotification(db, sess.UserId, notfId)
+	if err != nil {
+		c.Fail(500, err)
+		return
+	}
+
+	notf.Read = data.Read
+
+	c.JSON(200, notf)
 }
