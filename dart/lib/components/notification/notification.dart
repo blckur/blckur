96cc7c98ef3c4002f03916e4719e85ff606b682e
Zachary Huff <zach.huff.386@gmail.com>
2015-04-12 14:51:34 -0400

    Add on read to notification

--- dart/lib/components/notification/notification.dart
+++ dart/lib/components/notification/notification.dart
@@ -2,6 +2,8 @@ library notification_comp;
 
 import 'package:blckur/models/models.dart' as models;
 import 'package:blckur/utils/utils.dart' as utils;
+import 'package:blckur/logger.dart' as logger;
+import 'package:blckur/alert.dart' as alert;
 
 import 'package:angular/angular.dart' show Component, NgOneWay;
 
@@ -19,4 +21,17 @@ class NotificationComp {
 
   @NgOneWay('model')
   models.Notification model;
+
+  void onRead() {
+    if (!this.loading.set()) {
+      return;
+    }
+
+    this.model.markRead().catchError((err) {
+      logger.severe('Failed to mark notification as read', err);
+      new alert.Alert('Failed to mark notification as read');
+    }).whenComplete(() {
+      this.loading.clear();
+    });
+  }
 }
