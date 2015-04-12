e6656e62b5b99aca616e5564df36544508cd48f5
Zachary Huff <zach.huff.386@gmail.com>
2015-04-12 18:58:44 -0400

    Update decorators

--- dart/lib/decorators/decorators.dart
+++ dart/lib/decorators/decorators.dart
@@ -3,6 +3,7 @@ library decorators;
 import 'package:blckur/decorators/delay_click.dart' as delay_click;
 import 'package:blckur/decorators/noselect.dart' as noselect;
 import 'package:blckur/decorators/polymer.dart' as polymer;
+import 'package:blckur/decorators/touch_click.dart' as touch_click;
 
 import 'package:angular/angular.dart' as ng;
 
@@ -11,5 +12,6 @@ class DecoratorsMod extends ng.Module {
     this.bind(delay_click.DelayClickDec);
     this.bind(noselect.NoSelectDec);
     this.bind(polymer.PolymerDec);
+    this.bind(touch_click.TouchClickDec);
   }
 }
