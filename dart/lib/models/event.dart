a6ea1b2cc167074ebaaa48bd81a1c53b14c5cc36
Zachary Huff <zach.huff.386@gmail.com>
2015-04-13 00:42:03 -0400

    Add data to event model

--- dart/lib/models/event.dart
+++ dart/lib/models/event.dart
@@ -5,8 +5,7 @@ import 'package:blckur/model.dart' as model;
 class Event extends model.Model {
   String id;
   String type;
-  String resourceId;
-  double timestamp;
+  Map<String, dynamic> data;
 
   model.Model newModel() {
     return new Event();
@@ -16,8 +15,7 @@ class Event extends model.Model {
     return {
       'id': () => this.id,
       'type': () => this.type,
-      'resource_id': () => this.resourceId,
-      'timestamp': () => this.timestamp,
+      'data': () => this.data,
     };
   }
 
@@ -25,8 +23,7 @@ class Event extends model.Model {
     return {
       'id': (x) => this.id = x,
       'type': (x) => this.type = x,
-      'resource_id': (x) => this.resourceId = x,
-      'timestamp': (x) => this.timestamp = x,
+      'data': (x) => this.data = x,
     };
   }
 }
