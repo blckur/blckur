ca6a2f1a87b93cf21af139d52f7fc5d90094b021
Zachary Huff <zach.huff.386@gmail.com>
2015-04-12 21:00:15 -0400

    Replace ng-click with touch-click

--- dart/lib/components/alerts/alerts.dart
+++ dart/lib/components/alerts/alerts.dart
@@ -11,7 +11,7 @@ import 'dart:collection' as collection;
     'py-text="alert.text" duration="{{alert.duration}}" '
     'autoCloseDisabled="true" ng-class="getClass(\$index)" no-select><div '
     'class="retry" ng-if="alert.retryCallback != null" '
-    'ng-click="alert.retryCallback()">Retry</div></paper-toast>',
+    'touch-click="alert.retryCallback()">Retry</div></paper-toast>',
   cssUrl: 'packages/blckur/components/alerts/alerts.css'
 )
 class AlertsComp {
