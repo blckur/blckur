4631f92e7c8e1ce2c33a6f4aab783ae362c2a783
Zachary Huff <zach.huff.386@gmail.com>
2015-04-15 21:01:50 -0400

    Add email settings

---
+++ settings/email.go
@@ -0,0 +1,16 @@
+package settings
+
+var Email = &email{
+	Id: "email",
+}
+
+func init() {
+	register("email", Email)
+}
+
+type email struct {
+	Id       string `bson:"_id"`
+	Username string `bson:"username"`
+	Password string `bson:"password"`
+	Server   string `bson:"server"`
+}
