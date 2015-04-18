c8981e05f54a1131009285ae79f6a4c402c3e10e
Zachary Huff <zach.huff.386@gmail.com>
2015-04-18 18:00:05 -0400

    Remove get address in nodes

--- nodes/nodes.go
+++ nodes/nodes.go
@@ -1,18 +1,2 @@
 // Starting point for node types.
 package nodes
-
-import (
-	"github.com/Sirupsen/logrus"
-	"github.com/blckur/blckur/utils"
-)
-
-func getAddress() (address string) {
-	address, err := utils.GetLocalAddress()
-	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"error": err,
-		}).Error("queue: Failed to get ip")
-		panic(err)
-	}
-	return
-}
