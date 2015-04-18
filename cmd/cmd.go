923c830066a2d8cb34f7c2dea900709f2bee5622
Zachary Huff <zach.huff.386@gmail.com>
2015-04-18 02:03:19 -0400

    Remove database in cmd

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -6,7 +6,6 @@ import (
 	"math/rand"
 	"os"
 	"strconv"
-	"github.com/blckur/blckur/database"
 )
 
 type serviceOptions struct {
@@ -34,8 +33,6 @@ func getServiceOptions() (opts *serviceOptions) {
 	} else {
 		port = rand.Intn(55000) + 10000
 	}
-
-	database.MongoUrl = os.Getenv("DB")
 
 	opts = &serviceOptions{
 		Id:   id,
