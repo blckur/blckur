2110975a8aa2a7fc1ea53c3d0cc24dbe60be4101
Zachary Huff <zach.huff.386@gmail.com>
2015-04-30 18:32:21 -0400

    Add watch to requests

--- requests/requests.go
+++ requests/requests.go
@@ -2,6 +2,7 @@
 package requests
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/json"
 	"github.com/dropbox/godropbox/errors"
@@ -27,6 +28,57 @@ var (
 	DefaultTimeout = 10 * time.Second
 )
 
+type Event struct {
+	Type string
+	Data string
+}
+
+type Watcher struct {
+	scanner *bufio.Scanner
+	res     *http.Response
+	timeout time.Duration
+	Stream  chan *Event
+	Err     error
+}
+
+func (w *Watcher) scan() {
+	defer close(w.Stream)
+	defer w.Close()
+
+	var typ string
+	timer := time.AfterFunc(60 * time.Second, func() {
+		w.Close()
+	})
+
+	for w.scanner.Scan() {
+		data := w.scanner.Text()
+		n := len(data)
+
+		if n >= 7 && data[:7] == "event: " {
+			typ = data[7:]
+			continue
+		} else if n >= 6 && data[:6] == "data: " {
+			data = data[6:]
+		} else {
+			continue
+		}
+
+		if typ == "keep-alive" {
+			timer.Reset(60 * time.Second)
+			continue
+		}
+
+		w.Stream <- &Event{
+			Type: typ,
+			Data: data,
+		}
+	}
+}
+
+func (w *Watcher) Close() {
+	w.res.Body.Close()
+}
+
 type Request struct {
 	Method    string
 	Url       string
@@ -39,7 +91,7 @@ type Request struct {
 	Client    *http.Client
 }
 
-func (r *Request) Do() (resp *Response, err error) {
+func (r *Request) client() (client *http.Client) {
 	if r.Timeout == 0 {
 		r.Timeout = DefaultTimeout
 	}
@@ -53,11 +105,17 @@ func (r *Request) Do() (resp *Response, err error) {
 		}
 	}
 
-	client := r.Client
+	client = r.Client
 	if client == nil {
 		client = &http.Client{}
 	}
 	client.Transport = transport
+
+	return
+}
+
+func (r *Request) Do() (resp *Response, err error) {
+	client := r.client()
 
 	body := r.Body
 	if r.Json != nil {
@@ -105,6 +163,44 @@ func (r *Request) Do() (resp *Response, err error) {
 	resp = &Response{
 		res,
 	}
+
+	return
+}
+
+func (r *Request) Watch() (watcher *Watcher, err error) {
+	client := r.client()
+
+	req, err := http.NewRequest(r.Method, r.Url, nil)
+	if err != nil {
+		err = &ParseError{
+			errors.Wrap(err, "requests: Invalid request"),
+		}
+		return
+	}
+
+	if r.Header != nil {
+		req.Header = r.Header
+	}
+
+	req.Header.Add("Accept", "text/event-stream")
+
+	res, err := client.Do(req)
+	if err != nil {
+		err = &RequestError{
+			errors.Wrap(err, "requests: Request failed"),
+		}
+		return
+	}
+
+	watcher = &Watcher{
+		res: res,
+		scanner: bufio.NewScanner(res.Body),
+		Stream: make(chan *Event),
+	}
+
+	watcher.scanner.Split(bufio.ScanLines)
+
+	go watcher.scan()
 
 	return
 }
