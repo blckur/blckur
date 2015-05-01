3b34aa0d5103dafb0985642f6cd240611d1fdd18
Zachary Huff <zach.huff.386@gmail.com>
2015-05-01 03:14:54 -0400

    Add recovery function to handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,8 +2,11 @@
 package handlers
 
 import (
+	"fmt"
+	"github.com/Sirupsen/logrus"
 	"github.com/blckur/blckur/database"
 	"github.com/blckur/blckur/session"
+	"github.com/dropbox/godropbox/errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -52,10 +55,26 @@ func Session(required bool) gin.HandlerFunc {
 	}
 }
 
+func Recovery() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if r := recover(); r != nil {
+				logrus.WithFields(logrus.Fields{
+					"client": c.Request.RemoteAddr,
+					"error":  errors.New(fmt.Sprintf("%s", r)),
+				}).Error("handlers: Handler panic")
+				c.Writer.WriteHeader(http.StatusInternalServerError)
+			}
+		}()
+
+		c.Next()
+	}
+}
+
 // Register all endpoint handlers
 func Register(engine *gin.Engine, source string) {
 	engine.Use(Limiter)
-	engine.Use(gin.Recovery())
+	engine.Use(Recovery())
 
 	dbGroup := engine.Group("")
 	dbGroup.Use(Database)
