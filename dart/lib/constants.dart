1ad04ba75be5272df883feffd6d4b5ac4683872b
Zachary Huff <zach.huff.386@gmail.com>
2015-05-03 14:42:00 -0400

    Add constants

---
+++ dart/lib/constants.dart
@@ -0,0 +1,40 @@
+library constants;
+
+const WIDE = 0;
+const NARROW = 1;
+
+// Used for static hash versioning search and replace
+var icons = {
+  'blckur': {
+    'url': '/s/img/blckur.png',
+    'style': NARROW,
+  },
+  'digitalocean': {
+    'url': '/s/img/digitalocean.png',
+    'style': WIDE,
+  },
+  'github': {
+    'url': '/s/img/github.png',
+    'style': WIDE,
+  },
+  'gmail': {
+    'url': '/s/img/gmail.png',
+    'style': WIDE,
+  },
+  'hackernews': {
+    'url': '/s/img/hackernews.png',
+    'style': NARROW,
+  },
+  'hipchat': {
+    'url': '/s/img/hipchat.png',
+    'style': WIDE,
+  },
+  'stripe': {
+    'url': '/s/img/stripe.png',
+    'style': WIDE,
+  },
+  'twitter': {
+    'url': '/s/img/twitter.png',
+    'style': WIDE,
+  },
+};
