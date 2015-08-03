8d92e1b3622f10dc856452431dd77e7479bea1ac
Zachary Huff <zach.huff.386@gmail.com>
2015-08-02 21:13:24 -0400

    Move sign in oauth

--- oauth/oauth.go
+++ oauth/oauth.go
@@ -2,15 +2,7 @@
 package oauth
 
 import (
-	"crypto/hmac"
-	"crypto/sha1"
-	"encoding/base64"
-	"fmt"
-	"github.com/blckur/blckur/utils"
 	"labix.org/v2/mgo/bson"
-	"math/rand"
-	"strconv"
-	"time"
 )
 
 type Token struct {
@@ -18,41 +10,4 @@ type Token struct {
 	OauthSecret string        `bson:"oauth_secret"`
 	Type        string        `bson:"type"`
 	UserId      bson.ObjectId `bson:"user_id"`
-}
-
-func (c *Oauth1Client) Sign(method string, url string,
-	params map[string]string) (sig string) {
-	message := method + "&" + utils.Escape(url)
-	delimEsc := utils.Escape("&")
-	first := true
-
-	addParam := func(key string, val string) {
-		if first {
-			first = false
-			message += "&"
-		} else {
-			message += delimEsc
-		}
-		message += utils.Escape(fmt.Sprintf("%s=%s", key, val))
-	}
-
-	addParam("oauth_consumer_key", c.conf.ConsumerKey)
-	addParam("oauth_nonce", strconv.FormatInt(rand.Int63(), 10))
-	addParam("oauth_signature_method", "HMAC-SHA1")
-	addParam("oauth_timestamp", strconv.FormatInt(time.Now().Unix(), 10))
-	addParam("oauth_token", c.Token)
-	addParam("oauth_version", "1.0")
-
-	for key, val := range params {
-		addParam(key, val)
-	}
-
-	key := utils.Escape(c.conf.ConsumerSecret) + "&" + utils.Escape(c.Secret)
-
-	hashFunc := hmac.New(sha1.New, []byte(key))
-	hashFunc.Write([]byte(message))
-	rawSignature := hashFunc.Sum(nil)
-	sig = base64.StdEncoding.EncodeToString(rawSignature)
-
-	return
 }
--- oauth/oauth1.go
+++ oauth/oauth1.go
@@ -1,14 +1,22 @@
 package oauth
 
 import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"github.com/blckur/blckur/account"
 	"github.com/blckur/blckur/database"
 	"github.com/blckur/blckur/errortypes"
+	"github.com/blckur/blckur/utils"
 	"github.com/dropbox/godropbox/errors"
 	"github.com/mrjones/oauth"
 	"io/ioutil"
 	"labix.org/v2/mgo/bson"
+	"math/rand"
+	"strconv"
+	"time"
 )
 
 type Oauth1 struct {
@@ -156,6 +164,43 @@ func (c *Oauth1Client) GetJson(url string, userParams map[string]string,
 		}
 		return
 	}
+
+	return
+}
+
+func (c *Oauth1Client) Sign(method string, url string,
+	params map[string]string) (sig string) {
+	message := method + "&" + utils.Escape(url)
+	delimEsc := utils.Escape("&")
+	first := true
+
+	addParam := func(key string, val string) {
+		if first {
+			first = false
+			message += "&"
+		} else {
+			message += delimEsc
+		}
+		message += utils.Escape(fmt.Sprintf("%s=%s", key, val))
+	}
+
+	addParam("oauth_consumer_key", c.conf.ConsumerKey)
+	addParam("oauth_nonce", strconv.FormatInt(rand.Int63(), 10))
+	addParam("oauth_signature_method", "HMAC-SHA1")
+	addParam("oauth_timestamp", strconv.FormatInt(time.Now().Unix(), 10))
+	addParam("oauth_token", c.Token)
+	addParam("oauth_version", "1.0")
+
+	for key, val := range params {
+		addParam(key, val)
+	}
+
+	key := utils.Escape(c.conf.ConsumerSecret) + "&" + utils.Escape(c.Secret)
+
+	hashFunc := hmac.New(sha1.New, []byte(key))
+	hashFunc.Write([]byte(message))
+	rawSignature := hashFunc.Sum(nil)
+	sig = base64.StdEncoding.EncodeToString(rawSignature)
 
 	return
 }
